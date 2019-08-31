4a1c80e68f313938f351c48637a8597d9007024c
Sahil <sahilkhan0307.sk@gmail.com>
2019-08-31 12:03:00 +0530

    Add solution for problem `A - Carrot Cakes` Link: https://codeforces.com/contest/799/problem/A Tags: brute force, implementation, *1100 Ref: https://codeforces.com/contest/799/submission/59788924

---
+++ codeforces/799/A.cpp
@@ -0,0 +1,47 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long,long>
+#define pll pair<long long,long long>
+#define pld pair<long double,long double>
+#define gc getchar_unlocked
+#define pc putchar_unlocked
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define mod 1e9+7
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define vl vector<ll>
+#define mp make_pair
+
+int main()
+{
+    float n,t,k,d;
+    cin>>n>>t>>k>>d;
+    ll big = ceil(n/k)*t;
+    ll count=0,t1=0,t2=d;
+    t1+=t;count+=k;
+    while(count<n) {
+        if(t1<t2) 
+            t1+=t;
+        else 
+            t2+=t;
+        
+        count+=k;
+    }
+    ll sm=max(t1,t2);
+    cout<<((sm<big) ? "YES\n" : "NO\n");
+    return 0;
+}
