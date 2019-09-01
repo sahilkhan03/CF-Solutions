83395c188abb26406c04cd5cebafcf0a47f9e93f
Sahil <sahilkhan0307.sk@gmail.com>
2019-09-01 13:15:00 +0530

    Add solution for problem `A - Lineland Mail` Link: https://codeforces.com/contest/567/problem/A Tags: greedy, implementation, *900 Ref: https://codeforces.com/contest/567/submission/59842101

---
+++ codeforces/567/A.cpp
@@ -0,0 +1,41 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long,long>
+#define pll pair<long long,long long>
+#define pld pair<long double,long double>
+#define gc getchar_unlocked
+#define pc putchar_unlocked
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define mod 1e9+7
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define vl vector<ll>
+#define mp make_pair
+
+int main()
+{
+    ll n;
+    in(n);
+    vl v(n);
+    for(auto&i :v) in(i);
+    cout<<v[1]-v[0]<<" "<<v[n-1]-v[0]<<endl;
+    for(ll i=1;i<n-1;i++) {
+        cout<<min(v[i+1]-v[i], v[i]-v[i-1])<<" "<<max(v[n-1]-v[i],v[i]-v[0])<<endl;
+    }
+    cout<<v[n-1]-v[n-2]<<" "<<v[n-1]-v[0]<<endl;
+    return 0;
+}
