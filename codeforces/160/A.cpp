e7bbb4e120308499fe95c95433c3cae388059382
Sahil <sahilkhan0307.sk@gmail.com>
2019-09-01 19:21:00 +0530

    Add solution for problem `A - Twins` Link: https://codeforces.com/contest/160/problem/A Tags: greedy, sortings, *900 Ref: https://codeforces.com/contest/160/submission/59856811

---
+++ codeforces/160/A.cpp
@@ -0,0 +1,43 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long,long>
+#define pll pair<long long,long long>
+#define pld pair<long double,long double>
+#define gc getchar_unlocked
+#define pc putchar_unlocked
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define mod 1e9+7
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define vl vector<ll>
+#define mp make_pair
+
+int main()
+{
+    ll n,i,total=0,sum=0,flag=0;
+    in(n);
+    vl v(n);
+    for(auto& i:v) {in(i);total+=i;}
+    sort(v.begin(),v.end(),greater<int>());
+    for( i=0;i<v.size();i++) {
+        sum+=v[i];
+        if(sum>total/2) {flag=1;break;}
+    }
+    if(flag) cout<<i+1<<endl;
+    else cout<<i<<endl;
+    return 0;
+}
