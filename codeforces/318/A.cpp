3f9f7c2d0f0009038be90e3bba7040f49a105956
Sahil <sahilkhan0307.sk@gmail.com>
2019-09-01 20:36:00 +0530

    Add solution for problem `A - Even Odds` Link: https://codeforces.com/contest/318/problem/A Tags: math, *900 Ref: https://codeforces.com/contest/318/submission/59860421

---
+++ codeforces/318/A.cpp
@@ -0,0 +1,42 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long,long>
+#define pll pair<long long,long long>
+#define pld pair<long double,long double>
+#define gc getchar_unlocked
+#define pc putchar_unlocked
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define mod 1e9+7
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define vl vector<ll>
+#define mp make_pair
+
+int main()
+{
+    int64_t n,k;
+    cin>>n>>k;
+    if(n%2==0) {
+        if(k<=n/2) cout<<(k*2)-1<<endl;
+        else cout<<(k-(n/2))*2<<endl;
+    }
+    else  {
+        if(k<=((n/2)+1)) cout<<(k*2) - 1<<endl;
+        else cout<<(k-((n/2)+1))*2<<endl;
+    }
+    return 0;
+}
