5c0355958671a5fb200256a71f1288204edc4640
Sahil <sahilkhan0307.sk@gmail.com>
2019-09-04 22:18:00 +0530

    Add solution for problem `A - Good Number` Link: https://codeforces.com/contest/365/problem/A Tags: implementation, *1100 Ref: https://codeforces.com/contest/365/submission/60036049

---
+++ codeforces/365/A.cpp
@@ -0,0 +1,52 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long,long>
+#define pll pair<long long,long long>
+#define pld pair<long double,long double>
+#define gc getchar_unlocked
+#define pc putchar_unlocked
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define mod 1e9+7
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define vl vector<ll>
+#define mp make_pair
+
+int main()
+{
+    ll n,ans=0;
+    ll k;
+    cin>>n>>k;
+    while(n--) {
+        string s;
+        set<ll> c;
+        cin>>s;
+        for(ll i=0;i<s.size();i++) {
+            c.insert((ll)(s[i]-'0'));
+        }
+        bool flag=true;
+        for(ll i=0;i<=k;i++) {
+            if(c.find(i) == c.end()) {
+                flag = false;
+                break;
+            }
+        }
+        if(flag) ans++;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
