92b4af943831dd36bed2c6ad06dfa521925ee248
Sahil <sahilkhan0307.sk@gmail.com>
2019-09-09 12:20:00 +0530

    Add solution for problem `A - BowWow and the Timetable` Link: https://codeforces.com/contest/1204/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/1204/submission/60290453

---
+++ codeforces/1204/A.cpp
@@ -0,0 +1,42 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long,long>
+#define pll pair<long long,long long>
+#define pld pair<long double,long double>
+#define gc getchar_unlocked
+#define pc putchar_unlocked
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define mod 1e9+7
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define vl vector<ll>
+#define mp make_pair
+
+int main()
+{
+    string s;
+    cin>>s;
+    ll ans = s.size()/2;
+    bool flag=false;
+    for(ll i=1;i<s.size();i++) if(s[i]=='1') {
+        flag = 1; break;
+    }
+    
+    if(!flag or s.size()%2==0) cout<<ans<<endl;
+    else cout<<ans+1<<endl;
+    return 0;
+}
