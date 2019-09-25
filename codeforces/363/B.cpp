6a9b53b01fc4ad9da4e173c21c2250b2f440a564
Sahil <sahilkhan0307.sk@gmail.com>
2019-09-25 22:39:00 +0530

    Add solution for problem `B - Fence` Link: https://codeforces.com/contest/363/problem/B Tags: brute force, dp, *1100 Ref: https://codeforces.com/contest/363/submission/61281818

---
+++ codeforces/363/B.cpp
@@ -0,0 +1,47 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long,long>
+#define pll pair<long long,long long>
+#define pld pair<long double,long double>
+#define gc getchar_unlocked
+#define pc putchar_unlocked
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define mod 1e9+7
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define vl vector<ll>
+#define mp make_pair
+
+int main()
+{
+    ll n,k;
+    in2(n,k);
+    vl v(n);
+    ll ans=0,sum=0;
+    for(auto& i:v) in(i);
+    for(ll i=0;i<k;i++) sum+=v[i];
+    ll tmp=sum;
+    for(ll i=1,j=k;j<n;i++,j++) {
+        sum+=v[j];
+        sum-=v[i-1];
+        if(sum<tmp) {
+            ans=i; tmp=sum;
+        }
+    }
+    cout<<ans+1<<endl;
+    return 0;
+}
