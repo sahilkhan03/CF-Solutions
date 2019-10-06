193d70a66da24ae62dd84f910ca35e9419717f92
Sahil <sahilkhan0307.sk@gmail.com>
2019-10-06 20:49:00 +0530

    Add solution for problem `B - Strings Equalization` Link: https://codeforces.com/contest/1241/problem/B Tags: brute force, strings, *1000 Ref: https://codeforces.com/contest/1241/submission/61998913

---
+++ codeforces/1241/B.cpp
@@ -0,0 +1,54 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long,long>
+#define pll pair<long long,long long>
+#define pld pair<long double,long double>
+#define gc getchar_unlocked
+#define pc putchar_unlocked
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define mod 1e9+7
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define vl vector<ll>
+#define vpll vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+#define mp make_pair
+
+int main()
+{
+    unordered_map<char,bool> m;
+    ll q;
+    in(q);
+    while(q--) {
+        m.clear();
+        string s,t;
+        ll flag=0;
+        cin>>s>>t;
+        for(ll i=0;i<s.size();i++) {
+            m[s[i]] = true;
+        }
+        for(auto x:t) {
+            if(m.count(x)) {
+                cout<<"YES"<<endl;
+                flag=1;
+                break;
+            }
+        }
+        if(flag) continue;
+        cout<<"NO"<<endl;
+    }
+}
