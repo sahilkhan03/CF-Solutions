60bb24db12aa4652127397b92ee21b592d960174
sahil <sahilkhan0307.sk@gmail.com>
2019-10-12 18:54:00 +0530

    Add solution for problem `C - Vacation` Link: https://atcoder.jp/contests/dp/tasks/dp_c Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/7950114

--- atcoder/dp/C.cpp
+++ atcoder/dp/C.cpp
@@ -1,82 +1,48 @@
-/*
-    Author : DemonStar
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
 */
-
-// #pragma GCC optimize("Ofast")
-// #pragma GCC optimize("unroll-loops")
-// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx2,tune=native")
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define ordered_set tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update>
-#define fast                      \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define ll long long
-#define pi pair<int, int>
-#define pl pair<long long, long long>
-#define pld pair<long double, long double>
+#define pi pair<int,int>
+#define pl pair<long,long>
+#define pll pair<long long,long long>
+#define pld pair<long double,long double>
+#define gc getchar_unlocked
+#define pc putchar_unlocked
 #define endl '\n'
-#define loop(i, n) for (ll i = 0; i < n; i++)
-#define rep(i, begin, end) for (__typeof(begin) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
-#define all(x) x.begin(), x.end()
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define mod 1e9+7
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
 #define vl vector<ll>
-#define vi vector<int>
-#define ml map<ll, ll>
-#define vpl vector<pair<ll, ll>>
-const ll mod = 1e9 + 7;
-
-template <typename T, typename TT>
-ostream &operator<<(ostream &os, const pair<T, TT> &t)
-{
-    return os << t.first << " " << t.second;
-}
-template <typename T>
-ostream &operator<<(ostream &os, const vector<T> &t)
-{
-    for (auto &i : t)
-        os << i << " ";
-    return os;
-}
-template <typename T>
-istream &operator>>(istream &is, vector<T> &v)
-{
-    for (T &t : v)
-        is >> t;
-    return is;
-}
-template <typename T1, typename T2>
-istream &operator>>(istream &is, vector<pair<T1, T2>> &v)
-{
-    for (pair<T1, T2> &t : v)
-        is >> t.first >> t.second;
-    return is;
-}
+#define vpll vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+#define mp make_pair
 
 int main()
 {
-    fast;
-    int n;
-    cin >> n;
-    vector<vi> v(n, vi(3));
-    cin >> v;
-    vector<vi> dp(3, vi(n + 1, 0));
-    rep(i, 1, n + 1)
-    {
-        dp[0][i] = v[i - 1][0] + max(dp[1][i - 1], dp[2][i - 1]);
-        dp[2][i] = v[i - 1][2] + max(dp[1][i - 1], dp[0][i - 1]);
-        dp[1][i] = v[i - 1][1] + max(dp[0][i - 1], dp[2][i - 1]);
+    ll n;
+    in(n);
+    ll dp[3][n+1];
+    ll v[3][n];
+    for(ll i=0;i<n;i++) {
+        cin>>v[0][i]>>v[1][i]>>v[2][i];
     }
-    cout << max(dp[0][n], max(dp[1][n], dp[2][n])) << endl;
-
-#ifdef LOCAL
-    cerr
-        << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
-#endif
-
+    dp[0][0] = dp[1][0] = dp[2][0] = 0;
+    for(ll i=1;i<=n;i++) {
+        dp[0][i] = v[0][i-1] + max(dp[1][i-1],dp[2][i-1]);
+        dp[1][i] = v[1][i-1] + max(dp[0][i-1],dp[2][i-1]);
+        dp[2][i] = v[2][i-1] + max(dp[0][i-1],dp[1][i-1]);
+    }
+    cout<<max(dp[0][n],max(dp[1][n],dp[2][n]));
     return 0;
 }
