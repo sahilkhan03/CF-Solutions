1de5740c2f2b98953d2d3168bdb2aad83539c08d
Sahil <sahilkhan0307.sk@gmail.com>
2019-10-20 16:21:00 +0530

    Add solution for problem `C - Ivan the Fool and the Probability Theory` Link: https://codeforces.com/contest/1248/problem/C Tags: combinatorics, dp, math, *1700 Ref: https://codeforces.com/contest/1248/submission/63005427

---
+++ codeforces/1248/C.cpp
@@ -0,0 +1,44 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long,long>
+#define pll pair<long long,long long>
+#define pld pair<long double,long double>
+#define gc getchar_unlocked
+#define pc putchar_unlocked
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define mod ((ll)(1e9+7))
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define vl vector<ll>
+#define ml unordered_map<ll,ll> 
+#define vpll vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+#define mp make_pair
+
+ll dp[100000+5];
+
+int main()
+{
+   dp[0]=0;
+   dp[1]=1;
+   dp[2]=2;
+   for(ll i=3;i<100005;i++) dp[i]=(dp[i-1]+dp[i-2])%mod;
+    ll n,m;
+    in2(n,m);
+    cout<<(2*(dp[n]+dp[m]-1))%mod<<endl;
+    return 0;
+}
