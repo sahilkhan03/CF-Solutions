8dd130ac3497e26258b692e25d3c84a9a0cd00e0
Sahil <sahilkhan0307.sk@gmail.com>
2019-10-26 22:57:00 +0530

    Add solution for problem `C - p-binary` Link: https://codeforces.com/contest/1247/problem/C Tags: *1600 Ref: https://codeforces.com/contest/1247/submission/63501777

---
+++ codeforces/1247/C.cpp
@@ -0,0 +1,52 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long,long>
+#define pll pair<long long,long long>
+#define pld pair<long double,long double>
+#define gc getchar_unlocked
+#define pc putchar_unlocked
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define mod ((ll)(1e9+7))
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define vl vector<ll>
+#define ml unordered_map<ll,ll> 
+#define vpll vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+#define mp make_pair
+
+int main()
+{
+    ll n,p;
+    in2(n,p);
+    ll i=0;
+    while(true) {
+        ll diff = n - i*p;
+        if(diff<=0) {
+            cout<<-1<<endl;
+            return 0;
+        }
+        else if(__builtin_popcount(diff)<=i) {
+            if(diff<i)
+             cout<<-1<<endl;
+            else  cout<<i<<endl;
+            return 0;
+        }
+        i++;
+    }
+    return 0;
+}
