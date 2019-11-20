9ba16ee4d43d5b16c4ec9b0e42e92ada195cb6c0
Sahil <sahilkhan0307.sk@gmail.com>
2019-11-20 21:58:00 +0530

    Add solution for problem `B - New Year and Old Property` Link: https://codeforces.com/contest/611/problem/B Tags: bitmasks, brute force, implementation, *1300 Ref: https://codeforces.com/contest/611/submission/65447209

---
+++ codeforces/611/B.cpp
@@ -0,0 +1,49 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long,long>
+#define pll pair<long long,long long>
+#define pld pair<long double,long double>
+#define gc getchar_unlocked
+#define pc putchar_unlocked
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define mod ((ll)(1e9+7))
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define vl vector<ll>
+#define ml unordered_map<ll,ll> 
+#define vpll vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+#define mp make_pair
+
+
+int main() {
+    ll a,b;
+    in2(a,b);
+    ll ans=0;
+    for(ll i=0;(1LL<<i)<=2*b;i++) {
+        ll n = (1LL<<i) -1;
+        ll k = 0;
+        while (k<i-1)
+        {
+            ll tmp = n - (1LL<<k);
+            ans+=(tmp<=b and tmp>=a);
+            k++;
+        }   
+    }
+    cout<<ans<<endl;
+    return 0;
+}
