13c2cebce7b34002a4026ba26c7cda9428472cf5
Sahil <sahilkhan0307.sk@gmail.com>
2019-12-12 19:44:00 +0530

    Add solution for problem `C - Yet Another Broken Keyboard` Link: https://codeforces.com/contest/1272/problem/C Tags: combinatorics, dp, implementation, *1200 Ref: https://codeforces.com/contest/1272/submission/66696708

---
+++ codeforces/1272/C.cpp
@@ -0,0 +1,52 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long long,long long>
+#define pld pair<long double,long double>
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define mod ((ll)(1e9+7))
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define in3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define inv(v) for(auto&i:v) in(i)
+#define vl vector<ll>
+#define ml unordered_map<ll,ll> 
+#define vpl vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+
+int main() {
+    ll n,k;
+    in2(n,k);
+    string s;
+    cin>>s;
+    ml m;
+    loop(i,k) {
+        char t;
+        cin>>(t);
+        m[t]++;
+    };
+    ll ans=0,cur=0;
+    loop(i,n) {
+        if(!m[s[i]]) {
+            ans+=cur*(cur+1)/2;
+            cur=0;
+        }
+        else cur++;
+    }
+    ans+=cur*(cur+1)/2;
+    cout<<ans<<endl;
+    return 0;
+}
