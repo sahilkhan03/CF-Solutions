0e2d0bdbb82c094661bd7d3624b1b549eebbed87
Sahil <sahilkhan0307.sk@gmail.com>
2020-01-16 14:30:00 +0530

    Add solution for problem `B - Yet Another Meme Problem` Link: https://codeforces.com/contest/1288/problem/B Tags: math, *1100 Ref: https://codeforces.com/contest/1288/submission/68918876

---
+++ codeforces/1288/B.cpp
@@ -0,0 +1,54 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long long,long long>
+#define pld pair<long double,long double>
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);i++)
+#define mod ((ll)(1e9+7))
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define in3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define inv(v) for(auto&i:v) in(i)
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define ml unordered_map<ll,ll> 
+#define vpl vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+
+template<typename T, typename TT>
+ostream& operator<<(ostream &os, const pair<T, TT> &t) { return os<<t.first<<" "<<t.second; }
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T> &t) { for(auto& i: t) os<<i<<" "; return os; }
+
+int main() {
+    // fast;
+    ll t;
+    in(t);
+    while(t--) {
+        ll a,b;
+        in2(a,b);
+        ll d = log10(b)+1;
+        string c = to_string(b);
+        bool flag=0;
+        for(auto x:c) if(x!='9') {
+            flag=1;break;
+        }
+        if(flag) d--;
+        cout<<a*d<<endl;
+    }
+    return 0;
+}
