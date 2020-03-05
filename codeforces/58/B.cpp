db6bb362e4ff38f46d8143bb64f74cb91393a970
Sahil <sahilkhan0307.sk@gmail.com>
2020-03-05 11:31:00 +0530

    Add solution for problem `B - Coins` Link: https://codeforces.com/contest/58/problem/B Tags: greedy, *1300 Ref: https://codeforces.com/contest/58/submission/72489219

---
+++ codeforces/58/B.cpp
@@ -0,0 +1,56 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long long,long long>
+#define pld pair<long double,long double>
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);i++)
+#define mod ((ll)(1e9+7))
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define in3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define inv(v) for(auto&i:v) in(i)
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define ml unordered_map<ll,ll> 
+#define vpl vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+
+template<typename T, typename TT>
+ostream& operator<<(ostream &os, const pair<T, TT> &t) { return os<<t.first<<" "<<t.second; }
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T> &t) { for(auto& i: t) os<<i<<" "; return os; }
+
+ll func(ll n) {
+    for(ll i=n/2;i>1;i--) 
+        if(n%i==0) return i;
+    return -1;
+}
+
+int main() {
+    fast;
+    ll n;in(n);
+    vector<ll> ans;
+    if(n>1) ans.push_back(n);
+    while(n>1) {
+        n=func(n);
+        if(n==-1) break;
+        ans.push_back(n);
+    }
+    ans.push_back(1);
+    for(auto x:ans) cout<<x<<" ";
+    return 0;
+}
