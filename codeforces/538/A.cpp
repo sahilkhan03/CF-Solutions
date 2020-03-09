f500a9d208596e7ee48c363d56628e2afe198645
Sahil <sahilkhan0307.sk@gmail.com>
2020-03-09 12:59:00 +0530

    Add solution for problem `A - Cutting Banner` Link: https://codeforces.com/contest/538/problem/A Tags: brute force, implementation, *1400 Ref: https://codeforces.com/contest/538/submission/72769901

---
+++ codeforces/538/A.cpp
@@ -0,0 +1,55 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long long,long long>
+#define pld pair<long double,long double>
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);i++)
+#define mod ((ll)(1e9+7))
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define in3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define inv(v) for(auto&i:v) in(i)
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define ml unordered_map<ll,ll> 
+#define vpl vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+
+template<typename T, typename TT>
+ostream& operator<<(ostream &os, const pair<T, TT> &t) { return os<<t.first<<" "<<t.second; }
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T> &t) { for(auto& i: t) os<<i<<" "; return os; }
+
+
+int main() {
+    fast;
+    string s;
+    cin>>s;
+    string str="CODEFORCES";
+    ll l=0,r=s.size()-1,i=0;
+    while(s[l]==str[i] and i<str.size() and l<s.size()) l++,i++;
+    i=str.size()-1;
+    while(s[r]==str[i] and i>=0 and r>=0) r--,i--;
+    if(l+s.size()-r-1>=10) 
+        cout<<"YES"<<endl;
+    else 
+        cout<<"NO"<<endl;
+    #ifdef LOCAL_DEFINE
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+    return 0;
+}
