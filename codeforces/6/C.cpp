b9859ab48869ecb3eec39d60b48b2647c83f7ab6
Sahil <sahilkhan0307.sk@gmail.com>
2020-03-11 15:10:00 +0530

    Add solution for problem `C - Alice, Bob and Chocolate` Link: https://codeforces.com/contest/6/problem/C Tags: greedy, two pointers, *1200 Ref: https://codeforces.com/contest/6/submission/72943750

---
+++ codeforces/6/C.cpp
@@ -0,0 +1,68 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long long,long long>
+#define pld pair<long double,long double>
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);i++)
+#define mod ((ll)(1e9+7))
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define in3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define inv(v) for(auto&i:v) in(i)
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define ml unordered_map<ll,ll> 
+#define vpl vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+
+template<typename T, typename TT>
+ostream& operator<<(ostream &os, const pair<T, TT> &t) { return os<<t.first<<" "<<t.second; }
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T> &t) { for(auto& i: t) os<<i<<" "; return os; }
+
+int main() {
+    fast;
+    ll n;in(n);
+    vl v(n);
+    loop(i,n) in(v[i]);
+    if(n==1) {
+        cout<<1<<" "<<0<<endl;
+        return 0;
+    }
+    ll i=0,j=n-1;
+    ll a=0,b=0;
+    while(i<j) {
+        
+        if(v[i]==v[j])  {
+            a++,b++,i++,j--;
+            if(i==j) a++;
+        }
+        else if(v[i]<v[j])  {
+            v[j]-=v[i++],a++;
+            if(i==j) b++;
+        }
+        else {
+            v[i]-=v[j--],b++;
+            if(i==j) a++;
+        }
+    }
+    cout<<a<<" "<<b<<endl;
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+    return 0;
+}
