cb9da8a0e0dc5a3589235b55b3ee176c978450d1
Sahil <sahilkhan0307.sk@gmail.com>
2020-03-15 12:25:00 +0530

    Add solution for problem `D - Ehab the Xorcist` Link: https://codeforces.com/contest/1325/problem/D Tags: bitmasks, constructive algorithms, greedy, number theory, *1700 Ref: https://codeforces.com/contest/1325/submission/73310089

---
+++ codeforces/1325/D.cpp
@@ -0,0 +1,59 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long long,long long>
+#define pld pair<long double,long double>
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);i++)
+#define mod ((ll)(1e9+7))
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define in3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define inv(v) for(auto&i:v) in(i)
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define ml unordered_map<ll,ll> 
+#define vpl vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+
+template<typename T, typename TT>
+ostream& operator<<(ostream &os, const pair<T, TT> &t) { return os<<t.first<<" "<<t.second; }
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T> &t) { for(auto& i: t) os<<i<<" "; return os; }
+
+int main() {
+    fast;
+    ll u,v;
+    in2(u,v);
+    if(((u&1)^(v&1)) or u>v) {
+        cout<<-1<<endl;
+        return 0;
+    }
+    else if(!u and !v) {
+        cout<<0<<endl;
+        return 0;
+    }
+    ll x = (v-u)/2;
+    if(!x) 
+        cout<<1<<endl<<u<<endl;
+    else if(x&u)
+        cout<<3<<endl<<u<<" "<<x<<" "<<x<<endl;
+    else cout<<2<<endl<<x+u<<" "<<x<<endl;
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+    return 0;
+}
