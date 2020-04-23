37b225d9aafade4acd4719d6aa4d8728ff1b7be8
Sahil <sahilkhan0307.sk@gmail.com>
2020-04-23 18:34:00 +0530

    Add solution for problem `D - Constant Palindrome Sum` Link: https://codeforces.com/contest/1343/problem/D Tags: brute force, data structures, greedy, two pointers, *1700 Ref: https://codeforces.com/contest/1343/submission/77755315

---
+++ codeforces/1343/D.cpp
@@ -0,0 +1,57 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long long,long long>
+#define pld pair<long double,long double>
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);i++)
+#define mod ((ll)(1e9))
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define in3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define inv(v) for(auto&i:v) in(i)
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define ml unordered_map<ll,ll> 
+#define vpl vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+
+template<typename T, typename TT>
+ostream& operator<<(ostream &os, const pair<T, TT> &t) { return os<<t.first<<" "<<t.second; }
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T> &t) { for(auto& i: t) os<<i<<" "; return os; }
+
+int main() {
+    ll t;in(t);
+    while(t--) {
+        ll n,k;in2(n,k);
+        vl v(n),c(2*k+1,0),p(2*k+5,0);inv(v);
+        loop(i,n/2) c[v[i]+v[n-i-1]]++;
+        loop(i,n/2) p[min(v[i],v[n-i-1])+1]++,p[max(v[i],v[n-i-1])+k+1]--;
+        rep(i,1,2*k+3) p[i]+=p[i-1];
+        ll ans = 1e8;
+        rep(i,2,2*k) ans = min(ans, n-p[i]-c[i]);
+        cout<<ans<<endl;
+    }
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+    return 0;
+}
