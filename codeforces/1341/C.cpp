cc8e2dea12eeb76b5625c9059cdafedd5bf73a0b
Sahil <sahilkhan0307.sk@gmail.com>
2020-04-26 17:28:00 +0530

    Add solution for problem `C - Nastya and Strange Generator` Link: https://codeforces.com/contest/1341/problem/C Tags: greedy, implementation, *1500 Ref: https://codeforces.com/contest/1341/submission/78114107

---
+++ codeforces/1341/C.cpp
@@ -0,0 +1,70 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int>>, rb_tree_tag,tree_order_statistics_node_update>
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long long,long long>
+#define pld pair<long double,long double>
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);i++)
+#define mod ((ll)(1e9))
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define in3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define inv(v) for(auto&i:v) in(i)
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define ml unordered_map<ll,ll> 
+#define vpl vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+ 
+template<typename T, typename TT>
+ostream& operator<<(ostream &os, const pair<T, TT> &t) { return os<<t.first<<" "<<t.second; }
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T> &t) { for(auto& i: t) os<<i<<" "; return os; }
+ 
+int main() {
+    fast;
+    ll t; in(t);
+    while (t--) {
+        ll n;in(n); vl v(n), idx(n+1);inv(v);
+        loop(i,n) idx[v[i]] = i;
+        vl cnt(n,1); ll req=1;
+        while(req<=n) {
+            cnt[idx[req]]=0;
+            ll s = idx[req];
+            req++;
+            // cout<<s<<endl;
+            rep(j,s+1,n-1) {
+                if(!cnt[j]) break;
+                if(v[j]!=req) {
+                    cout<<"No"<<endl;
+                    goto skip;
+                }
+                // cout<<j<<" "<<req<<" "<<v[j]<<endl;
+                cnt[j]=0; req++;
+            }
+        }
+        cout<<"Yes"<<endl;
+        skip:;
+    }
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+    return 0;
+}
