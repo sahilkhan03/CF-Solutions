c6abafbaf32cd3615a09e0287ac1e25a11d4818c
Sahil <sahilkhan0307.sk@gmail.com>
2020-04-28 18:38:00 +0530

    Add solution for problem `C - Yet Another Counting Problem` Link: https://codeforces.com/contest/1342/problem/C Tags: math, number theory, *1600 Ref: https://codeforces.com/contest/1342/submission/78385393

---
+++ codeforces/1342/C.cpp
@@ -0,0 +1,73 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update>
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long long,long long>
+#define pld pair<long double,long double>
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);i++)
+#define mod ((ll)(1e9))
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define in3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define inv(v) for(auto&i:v) in(i)
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define ml unordered_map<ll,ll> 
+#define vpl vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+ 
+template<typename T, typename TT>
+ostream& operator<<(ostream &os, const pair<T, TT> &t) { return os<<t.first<<" "<<t.second; }
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T> &t) { for(auto& i: t) os<<i<<" "; return os; }
+
+ll func(ll n, ll a, ll b) {
+    if(n<b) return 0;
+    ll ans = 0, lcm = b/__gcd(a,b);lcm*=a;
+    if(n<lcm) {
+        return n-b+1;
+    }
+    ans+=lcm-b;n-=lcm;
+    ll nlcm=n/lcm, rem = n%lcm;
+    if(n%lcm) rem++;
+    ans+=(lcm-b)*nlcm;
+    ll left = (rem+b-1)/b;
+    // cout<<"---"<<left<<endl;
+    if(rem>b) ans+=rem-b;
+    return ans;
+}
+
+int main() {
+    fast;
+    ll t; in(t);
+    while (t--) {
+        ll a,b,q; in3(a,b,q);
+        if(b<a) swap(a,b);
+        while(q--) {
+            ll l,r; in2(l,r);
+            cout<<func(r,a,b) - func(l-1,a,b)<<" ";        
+        }
+        cout<<endl;
+    }
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+    return 0;
+}
