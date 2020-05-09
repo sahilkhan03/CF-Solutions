d838223d80ab8a4e84e4ca3cc419e999cf20be06
Sahil <sahilkhan0307.sk@gmail.com>
2020-05-09 22:27:00 +0530

    Add solution for problem `F - Binary String Reconstruction` Link: https://codeforces.com/contest/1352/problem/F Tags: constructive algorithms, dfs and similar, math, *1500 Ref: https://codeforces.com/contest/1352/submission/79572396

---
+++ codeforces/1352/F.cpp
@@ -0,0 +1,71 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long long,long long>
+#define pld pair<long double,long double>
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define rep(i, begin, end) for (__typeof(begin) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define mod ((ll)(1e9+7))
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define in3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define inv(v) for(auto&i:v) in(i)
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define ml unordered_map<ll,ll> 
+#define vpl vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+ 
+template<typename T, typename TT>
+ostream& operator<<(ostream &os, const pair<T, TT> &t) { return os<<t.first<<" "<<t.second; }
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T> &t) { for(auto& i: t) os<<i<<" "; return os; }
+
+int main() {
+    fast;
+    int t; scanf("%d",&t);
+    while(t--) {
+        ll n0,n1,n2; in3(n0,n1,n2);
+        string a0="",a1="",a2="";
+        if(n0 and n2) n1--;
+        else if(!n0 and !n2) {
+            if(n1) {
+                a1+="10", n1--;
+            }
+            while(n1--) if(a1.back()-'0') a1+='0'; else a1+='1';  
+            cout<<a1<<endl;
+            continue;
+        }
+        int f=0;
+        if(!n0) f=1; else if(!n2) f=2;
+        if(n0) while(n0>=0) a0+="0",n0--;
+        if(n2) while(n2>=0) a2+='1',n2--;
+        while(n1>=2) a1+="10",n1-=2;
+        if(n1) {
+            if(f==2) a0='1'+a0;
+            else a2+="0";
+        }
+        cout<<a0<<a1<<a2<<endl;
+    }
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+    return 0;
+}
