32057fc44b05a456d4eee046aa08998b6d8f2ac3
Sahil <sahilkhan0307.sk@gmail.com>
2020-05-17 12:41:00 +0530

    Add solution for problem `B - Young Explorers` Link: https://codeforces.com/contest/1355/problem/B Tags: dp, greedy, sortings, *1200 Ref: https://codeforces.com/contest/1355/submission/80442956

---
+++ codeforces/1355/B.cpp
@@ -0,0 +1,88 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<pl, null_type, less<pl>, rb_tree_tag, tree_order_statistics_node_update>
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define pi pair<int, int>
+#define pl pair<long long, long long>
+#define pld pair<long double, long double>
+#define endl '\n'
+#define loop(i, n) for (ll i = 0; i < n; i++)
+#define rep(i, begin, end) for (__typeof(begin) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define mod ((ll)(1e9 + 7))
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define ml map<ll, ll>
+#define vpl vector<pair<ll, ll>>
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t)
+{
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t)
+{
+    for (auto &i : t)
+        os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (T &t : v)
+        is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v)
+{
+    for (pair<T1, T2> &t : v)
+        is >> t.first >> t.second;
+    return is;
+}
+
+int main()
+{
+    fast;
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vl v(n);
+        cin >> v;
+        ll ans = 0, rem = 0, last = 1e18;
+        ml m;
+        loop(i, n) m[v[i]]++;
+        for (auto x : m)
+        {
+            ans += x.second / x.first, rem += x.second % x.first;
+            if (x.second % x.first)
+                last = x.first;
+            if (rem >= last)
+                ans++, rem %= last, last = 1e18;
+        }
+        cout << ans << endl;
+    }
+
+#ifdef LOCAL
+    cerr
+        << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+
+    return 0;
+}
