88941531e680b3a8808d20b9a8006cf888db9ddd
Sahil <sahilkhan0307.sk@gmail.com>
2020-05-18 16:01:00 +0530

    Add solution for problem `D - Multiset` Link: https://codeforces.com/contest/1354/problem/D Tags: binary search, data structures, *1900 Ref: https://codeforces.com/contest/1354/submission/80592102

---
+++ codeforces/1354/D.cpp
@@ -0,0 +1,114 @@
+/*                    ___          |\    /|           ____   ____   ____  ____
+   |     |    /\    /    \ |     | | \  / |    /\    |    \ |    \ |     |    \
+   |_____|   /__\   \____  |_____| |  \/  |   /__\   |____/ |____/ |__   |____/
+   |     |  /    \       \ |     | |      |  /    \  |      |      |     |   \
+   |     | /      \ \____/ |     | |      | /      \ |      |      |____ |    \
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update>
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define pi pair<int, int>
+#define pl pair<long long, long long>
+#define pld pair<long double, long double>
+#define endl '\n'
+#define loop(i, n) for (ll i = 0; i < n; i++)
+#define rep(i, begin, end) for (__typeof(begin) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define mod ((ll)(1e9 + 7))
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define ml map<ll, ll>
+#define vpl vector<pair<ll, ll>>
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t)
+{
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t)
+{
+    for (auto &i : t)
+        os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (T &t : v)
+        is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v)
+{
+    for (pair<T1, T2> &t : v)
+        is >> t.first >> t.second;
+    return is;
+}
+
+const int N = 1e6 + 1;
+int fen[N];
+void update(int i, int s)
+{
+    while (i < N)
+        fen[i] += s, i += (i & (-i));
+}
+int sum(int i)
+{
+    int s = 0;
+    while (i)
+        s += fen[i], i -= i & (-i);
+    return s;
+}
+int search(int n)
+{
+    int pos = 0, sum = 0;
+    for (int i = log2(N); i >= 0; i--)
+    {
+        if (pos + (1 << i) < N and sum + fen[pos + (1 << i)] < n)
+            pos += 1 << i, sum += fen[pos];
+    }
+    return pos + 1;
+}
+
+int main()
+{
+    fast;
+    int n, q, last, t;
+    scanf("%d%d", &n, &q);
+    loop(i, n)
+    {
+        scanf("%d", &t);
+        update(t, 1);
+    }
+    while (q--)
+    {
+        scanf("%d", &t);
+        if (t > 0)
+            update(t, 1);
+        else
+        {
+            t = -t;
+            last = search(t);
+            update(last, -1);
+        }
+    }
+    if (sum(N - 1))
+        printf("%d", search(1));
+    else
+        printf("0");
+#ifdef LOCAL
+    cerr
+        << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+
+    return 0;
+}
