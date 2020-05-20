1df93c06bac5c7f8664d804167caf8698cea457a
sahil <sahilkhan0307.sk@gmail.com>
2020-05-20 22:10:00 +0530

    Add solution for problem `F - LCS` Link: https://atcoder.jp/contests/dp/tasks/dp_f Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/13438653

--- atcoder/dp/F.cpp
+++ atcoder/dp/F.cpp
@@ -61,56 +61,54 @@ int main()
 {
     string a, b;
     cin >> a >> b;
-    vector<vector<vi>> dp(a.size() + 1, vector<vi>(b.size() + 1, vi(3)));
+    vector<vector<vi>> dp(a.size(), vector<vi>(b.size(), vi(5)));
+    if (a[0] == b[0])
+        dp[0][0] = {1, 0, 0, -1, -1};
+    else
+        dp[0][0] = {0, -1, -1, -1, -1};
     for (int i = 0; i < a.size(); i++)
     {
         for (int j = 0; j < b.size(); j++)
         {
             if (!i and !j)
+                continue;
+            if (!i)
             {
-                dp[i][j] = {(a[i] == b[j]), -1, -1};
-            }
-            else if (!i)
-            {
+                dp[i][j] = dp[i][j - 1];
                 if (a[i] == b[j])
-                    dp[i][j] = {1, -1, -1};
-                else
-                    dp[i][j] = {dp[i][j - 1][0], i, j - 1};
+                    dp[i][j] = {1, i, j, -1, -1};
             }
             else if (!j)
             {
+                dp[i][j] = dp[i - 1][j];
                 if (a[i] == b[j])
-                    dp[i][j] = {1, -1, -1};
-                else
-                    dp[i][j] = {dp[i - 1][j][0], i - 1, j};
+                    dp[i][j] = {1, i, 0, -1, -1};
             }
             else
             {
                 if (a[i] == b[j])
                 {
-                    dp[i][j] = {dp[i - 1][j - 1][0] + 1, i - 1, j - 1};
+                    dp[i][j] = {dp[i - 1][j - 1][0] + 1, i, j, dp[i - 1][j - 1][1], dp[i - 1][j - 1][2]};
                 }
                 else if (dp[i - 1][j] > dp[i][j - 1])
                 {
-                    dp[i][j] = {dp[i - 1][j][0], i - 1, j};
+                    dp[i][j] = dp[i - 1][j];
                 }
                 else
-                    dp[i][j] = {dp[i][j - 1][0], i, j - 1};
+                    dp[i][j] = dp[i][j - 1];
             }
         }
     }
     string ans = "";
     auto cur = dp[a.size() - 1][b.size() - 1];
-    int i = a.size() - 1, j = b.size() - 1;
-    while (true)
+    // cout << cur[0] << endl;
+    while (cur[1] != -1 or cur[2] != -1)
     {
+        ans = a[cur[1]] + ans;
         // cout << cur << endl;
-        if (a[i] == b[j])
-            ans = a[i] + ans;
-        if (cur[1] == -1 or cur[2] == -1)
+        if (cur[3] == -1 or cur[4] == -1)
             break;
-        i = cur[1], j = cur[2];
-        cur = dp[i][j];
+        cur = dp[cur[3]][cur[4]];
     }
     cout << ans << endl;
 
