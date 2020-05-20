8a471f7bcb00ceda87d61cdc65bc32c797265212
sahil <sahilkhan0307.sk@gmail.com>
2020-05-20 22:45:00 +0530

    Add solution for problem `F - LCS` Link: https://atcoder.jp/contests/dp/tasks/dp_f Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/13439094

--- atcoder/dp/F.cpp
+++ atcoder/dp/F.cpp
@@ -2,7 +2,7 @@
     Author : DemonStar
 */
 
-//#pragma GCC optimize("Ofast")
+#pragma GCC optimize("Ofast")
 //#pragma GCC optimize("unroll-loops")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx2,tune=native")
 
