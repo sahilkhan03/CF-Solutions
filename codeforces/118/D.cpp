6de7f4f307431711f9f0637ac09a3a86999fd08d
Sahil <sahilkhan0307.sk@gmail.com>
2020-05-24 14:44:00 +0530

    Add solution for problem `D - Caesar's Legions` Link: https://codeforces.com/contest/118/problem/D Tags: dp, *1700 Ref: https://codeforces.com/contest/118/submission/81153154

--- codeforces/118/D.cpp
+++ codeforces/118/D.cpp
@@ -2,112 +2,100 @@
     Author : DemonStar
 */
 
-#pragma GCC optimize("Ofast")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("unroll-loops")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx2,tune=native")
+
 #include <bits/stdc++.h>
+using namespace std;
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-using namespace std;
 using namespace __gnu_pbds;
-typedef long long ll;
-#define ordered_set tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
-#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define all(x) x.begin(), x.end()
-#define loop(i, n) for(ll i = 0; i < n; i++)
-#define pl pair<ll, ll>
-#define vl vector<ll>
+#define ordered_set tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update>
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define pi pair<int, int>
+#define pl pair<long long, long long>
+#define pld pair<long double, long double>
 #define endl '\n'
-void __print(int x) {cerr << x;}
-void __print(long x) {cerr << x;}
-void __print(long long x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(long double x) {cerr << x;}
-void __print(char x) {cerr << '\'' << x << '\'';}
-void __print(const char *x) {cerr << '\"' << x << '\"';}
-void __print(const string &x) {cerr << '\"' << x << '\"';}
-void __print(bool x) {cerr << (x ? "true" : "false");}
-
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
-void _print() {cerr << "]\n";}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-
-#ifdef LOCAL
-#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
-#else
-#define debug(x...)
-#endif
-
-struct custom_hash {
-    static uint64_t splitmix64(uint64_t x) {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-    size_t operator()(uint64_t x) const {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
-
+#define loop(i, n) for (ll i = 0; i < n; i++)
+#define rep(i, begin, end) for (__typeof(begin) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define ml map<ll, ll>
+#define vpl vector<pair<ll, ll>>
+const ll mod = 1e9 + 7;
 
 template <typename T, typename TT>
-ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+ostream &operator<<(ostream &os, const pair<T, TT> &t)
+{
     return os << t.first << " " << t.second;
 }
 template <typename T>
-ostream &operator<<(ostream &os, const vector<T> &t) {
-    for (auto &i : t) os << i << " ";
+ostream &operator<<(ostream &os, const vector<T> &t)
+{
+    for (auto &i : t)
+        os << i << " ";
     return os;
 }
 template <typename T>
-istream &operator>>(istream &is, vector<T> &v) {
-    for (T &t : v) is >> t;
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (T &t : v)
+        is >> t;
     return is;
 }
 template <typename T1, typename T2>
-istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
-    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v)
+{
+    for (pair<T1, T2> &t : v)
+        is >> t.first >> t.second;
     return is;
 }
 
-const ll mod = 1e8;
+ll modpow(ll n, ll p) {
+    ll ans = 1;
+    while(p) {
+        if(p & 1) (ans *= n) %= mod;
+        (n *= n) %= mod, p /= 2;
+    }
+    return ans;
+}
+
+ll dp[105][105][11][11];
+// string cur = "";
+int k11, k22;
+ll  calc(int n1, int n2, int k1, int k2) {
+    if(!n1 and !n2) {
+        return 1;
+    }
+    ll &ans = dp[n1][n2][k1][k2];
+    if(ans != -1) return ans;
+    ans = 0;
+    if(n1 and k1) {
+        (ans += calc(n1 - 1, n2, k1 -1, k22)) %= 100000000;
+    } 
+    if(n2 and k2) {
+        (ans += calc(n1,  n2 - 1, k11, k2 - 1)) %= 100000000;
+    }
+    return ans;
+}
 
 int main()
 {
+    fast;
+    int n1, n2;
+    memset(dp, -1, sizeof(dp));
+    cin >> n1 >> n2 >> k11 >> k22;
+    cout << calc(n1, n2, k11, k22) << endl;
 
-#ifdef LOCAL
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-
-    ll n1, n2, k1, k2;
-    cin >> n1 >> n2 >> k1 >> k2;
-    ll dp[2][n1 + 1][n2 + 1];
-    memset(dp, 0, sizeof dp);
-    dp[0][0][0] = 1;
-    dp[1][0][0] = 1;
-    for (int i = 0; i <= n1; i++) {
-        for (int j = 0; j <= n2; j++) {
-            for (int k = 1; k <= k1 and i - k >= 0; k++)
-                (dp[0][i][j] += dp[1][i - k][j]) %= mod;
-            for (int k = 1; k <= k2 and j - k >= 0; k++)
-                (dp[1][i][j] += dp[0][i][j - k]) %= mod;
-            debug(i, j, dp[0][i][j], dp[1][i][j]);
-        }
-    }
-    cout << (dp[0][n1][n2]  + dp[1][n1][n2]) % mod << endl;
-
-#ifdef LOCAL
-    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s";
-#endif
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
 
     return 0;
 }
