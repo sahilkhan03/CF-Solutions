f9d85f78d7a50493ad30f929a57ff9a28b77a257
Sahil <sahilkhan0307.sk@gmail.com>
2020-05-24 18:52:00 +0530

    Add solution for problem `E - Let's Go Rolling!` Link: https://codeforces.com/contest/38/problem/E Tags: dp, sortings, *1800 Ref: https://codeforces.com/contest/38/submission/81201686

--- codeforces/38/E.cpp
+++ codeforces/38/E.cpp
@@ -3,7 +3,7 @@
 */
 
 #pragma GCC optimize("Ofast")
-// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
 
 #include <bits/stdc++.h>
 using namespace std;
