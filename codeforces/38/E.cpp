07b9da26672de830f595d7fd117ceac02771143b
Sahil <sahilkhan0307.sk@gmail.com>
2020-05-24 18:53:00 +0530

    Add solution for problem `E - Let's Go Rolling!` Link: https://codeforces.com/contest/38/problem/E Tags: dp, sortings, *1800 Ref: https://codeforces.com/contest/38/submission/81201729

---
+++ codeforces/38/E.cpp
@@ -0,0 +1,84 @@
+/*
+    Author : DemonStar
+*/
+
+#pragma GCC optimize("Ofast")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update>
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define pi pair<int, int>
+#define pl pair<long long, long long>
+#define pld pair<long double, long double>
+#define endl '\n'
+#define loop(i, n) for (ll i = 0; i < n; i++)
+#define rep(i, begin, end) for (__typeof(begin) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define ml map<ll, ll>
+#define vpl vector<pair<ll, ll>>
+const ll mod = 1e9 + 7;
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t)
+{
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t)
+{
+    for (auto &i : t)
+        os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (T &t : v)
+        is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v)
+{
+    for (pair<T1, T2> &t : v)
+        is >> t.first >> t.second;
+    return is;
+}
+
+vector<vl> dp(3005, vl(3005, -1e18));
+ll n;
+
+ll calc(vpl &v, ll i, ll last) {
+    if(i >= n) 
+        return 0;
+    ll &ans = dp[i][last];
+    if(ans != -1e18) return ans;
+    ans = min(v[i].second + calc(v, i + 1, i), v[i].first - v[last].first + calc(v, i + 1, last));
+    return ans;
+}
+
+int main()
+{
+    fast;
+    cin >> n;
+    vpl v(n);
+    cin >> v;
+    sort(all(v));
+    cout << calc(v, 1, 0) + v[0].second << endl;
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+
+    return 0;
+}
