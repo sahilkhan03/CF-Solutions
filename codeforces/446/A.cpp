e74da66be7a07d4b1f5e4a08be37f1a589a13375
Sahil <sahilkhan0307.sk@gmail.com>
2020-05-27 21:49:00 +0530

    Add solution for problem `A - DZY Loves Sequences` Link: https://codeforces.com/contest/446/problem/A Tags: dp, implementation, two pointers, *1600 Ref: https://codeforces.com/contest/446/submission/81638401

---
+++ codeforces/446/A.cpp
@@ -0,0 +1,99 @@
+/*
+    Author : DemonStar
+*/
+
+#pragma GCC optimize("Ofast")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx2,tune=native")
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update>
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define pi pair<int, int>
+#define pl pair<long long, long long>
+#define pld pair<long double, long double>
+#define endl '\n'
+#define loop(i, n) for (ll i = 0; i < n; i++)
+#define rep(i, begin, end) for (__typeof(begin) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define ml map<ll, ll>
+#define vpl vector<pair<ll, ll>>
+const ll mod = 1e9 + 7;
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t)
+{
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t)
+{
+    for (auto &i : t)
+        os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (T &t : v)
+        is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v)
+{
+    for (pair<T1, T2> &t : v)
+        is >> t.first >> t.second;
+    return is;
+}
+
+int main()
+{
+
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    fast;
+    int n;
+    cin >> n;
+    vl v(n);
+    cin >> v;
+    if (n < 3) {
+        cout << n << endl;
+        return 0;
+    }
+    vl pre(n), suf(n);
+    pre[0] = 1, suf[n - 1] = 1;
+    rep(i, 1, n) {
+        pre[i] = 1 + (v[i] > v[i - 1]) * pre[i - 1];
+    }
+    for (int i = n - 2; ~i ; i--)
+        suf[i] = 1 + (v[i] < v[i + 1]) * suf[i + 1];
+    ll ans = *max_element(all(pre));
+    ans = max(ans, max(suf[1] + 1, 1 + pre[n - 2]));
+    for (int i = 1; i < n - 1; i++) {
+        if (v[i + 1] - v[i - 1] > 1)
+            ans = max(ans , pre[i - 1] + suf[i + 1] + 1);
+        else
+            ans = max(ans, max(pre[i - 1] + 1, 1 + suf[i + 1]));
+    }
+
+    cout << ans << endl;
+
+#ifdef LOCAL
+    fprintf(stderr, "Time elapsed: %lf s\n", 1.0 * clock() / CLOCKS_PER_SEC);
+#endif
+
+    return 0;
+}
