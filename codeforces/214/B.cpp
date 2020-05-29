0f9125c30c95d5704bcc4579bef9d63b2655abaf
Sahil <sahilkhan0307.sk@gmail.com>
2020-05-29 12:44:00 +0530

    Add solution for problem `B - Hometask` Link: https://codeforces.com/contest/214/problem/B Tags: brute force, constructive algorithms, greedy, math, *1600 Ref: https://codeforces.com/contest/214/submission/81858060

---
+++ codeforces/214/B.cpp
@@ -0,0 +1,155 @@
+/*
+    Author : DemonStar
+*/
+
+#pragma GCC optimize("Ofast")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx2,tune=native")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+#define ordered_set tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update>
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define loop(i, n) for(ll i = 0; i < n; i++)
+#define endl '\n'
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto &i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+const ll mod = 1e9 + 7;
+
+int main()
+{
+
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    fast;
+    int n;
+    cin >> n;
+    bool f1 = 0;
+    string s = "";
+    ll sum = 0;
+    loop(i, n) {
+        int t;
+        cin >> t;
+        sum += t;
+        s += char(t + '0');
+        if (!t) f1 = 1;
+    }
+    if (!f1) {
+        cout << -1 << endl;
+        return 0;
+    }
+    sort(all(s));
+    int c = 2;
+    if (sum % 3 == 1) {
+        loop(i, s.size()) {
+            if ((s[i] - '0') % 3 == 1) {
+                s.erase(s.begin() + i);
+                goto skip;
+            }
+        }
+        while (c--) {
+            loop(i, s.size()) {
+                if ((s[i] - '0') % 3 == 2) {
+                    s.erase(s.begin() + i);
+                    break;
+                }
+            }
+        }
+    }
+    else if (sum % 3 == 2) {
+        loop(i, s.size()) {
+            if ((s[i] - '0') % 3 == 2) {
+                s.erase(s.begin() + i);
+                goto skip;
+            }
+        }
+        while (c--) {
+            loop(i, s.size()) {
+                if ((s[i] - '0') % 3 == 1) {
+                    s.erase(s.begin() + i);
+                    break;
+                }
+            }
+        }
+skip:;
+    }
+    reverse(all(s));
+    if (s[0] - '0')
+        cout << s << endl;
+    else
+        cout << 0 << endl;
+
+
+#ifdef LOCAL
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s\n";
+#endif
+
+    return 0;
+}
