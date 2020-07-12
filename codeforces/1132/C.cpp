88c7679b06c52dd1bbc8bc5d3c68ce7db856f303
Sahil <sahilkhan0307.sk@gmail.com>
2020-07-12 16:47:00 +0530

    Add solution for problem `C - Painting the Fence` Link: https://codeforces.com/contest/1132/problem/C Tags: brute force, *1700 Ref: https://codeforces.com/contest/1132/submission/86643966

---
+++ codeforces/1132/C.cpp
@@ -0,0 +1,124 @@
+/*
+    Author : DemonStar
+*/
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target ("sse4")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+#define ordered_set tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define loop(i, n) for(ll i = 0; i < n; i++)
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define endl '\n'
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto &i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+const ll mod = 1e9 + 7;
+
+
+int main()
+{
+
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    fast;
+    short n, m;
+    cin >> n >> m;
+    vector<pair<short, short>> v(m);
+    cin >> v;
+    vector<vector<short>> pt(n + 1, vector<short>());
+    for (int c = 0; c < m; c++) {
+        for (int i = v[c].first; i <= v[c].second; i++)
+            pt[i].push_back(c);
+    }
+    vector<vector<short>> cnt(m, vector<short>(m, 0));
+    vector<int> sg(m, 0);
+    ll sum  = 0;
+    for (auto x : pt) {
+        if (x.empty()) continue;
+        if (x.size() == 1) sg[x[0]]++;
+        else if (x.size() == 2) cnt[x[0]][x[1]]++, cnt[x[1]][x[0]]++;
+        sum++;
+    }
+    ll ans = 0;
+    for (int i = 0; i < m; i++) {
+        for (int j = i + 1; j < m; j++) {
+            ans = max(ans, sum - sg[i] - sg[j] - cnt[i][j]);
+        }
+    }
+    cout << ans << endl;
+
+#ifdef LOCAL
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s";
+#endif
+
+    return 0;
+}
