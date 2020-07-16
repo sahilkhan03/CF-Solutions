b7509d473709ba01ff1c87d4981e1ddf3ea43e1c
Sahil <sahilkhan0307.sk@gmail.com>
2020-07-16 15:52:00 +0530

    Add solution for problem `D - GCD Counting` Link: https://codeforces.com/contest/1101/problem/D Tags: data structures, dfs and similar, dp, number theory, trees, *2000 Ref: https://codeforces.com/contest/1101/submission/86977901

---
+++ codeforces/1101/D.cpp
@@ -0,0 +1,126 @@
+/*
+    Author : DemonStar
+*/
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target ("sse4")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define loop(i, n) for(ll i = 0; i < n; i++)
+#define mset(v, a) memset(v, a, sizeof v);
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto &i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+const ll mod = 1e9 + 7;
+
+ll ans = 0;
+vector<map<ll, ll>> dist;
+void dfs(vector<vl> &g, vl &v, ll i, ll prev) {
+    debug(i);
+    for (auto j : g[i]) {
+        if (j == prev) continue;
+        dfs(g, v, j, i);
+        if ( __gcd(v[i], v[j]) == 1) continue;
+        for (auto x : dist[i]) {
+            for (auto y : dist[j]) {
+                if (__gcd(x.first, y.first) != 1)
+                    ans = max(ans, x.second + y.second);
+            }
+        }
+        for (auto x : dist[j]) {
+            ll gcd = __gcd(x.first, v[i]);
+            if (gcd != 1)
+                dist[i][gcd] = max(dist[i][gcd], x.second + 1);
+        }
+    }
+}
+
+int main()
+{
+
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    fast;
+    ll n;
+    cin >> n;
+    vl v(n);
+    dist.resize(n);
+    cin >> v;
+    if (*max_element(all(v)) > 1) ans = 1;
+    vector<vl> g(n);
+    for (int i = 0; i < n - 1; i++ ) {
+        ll a, b;
+        cin >> a >> b;
+        a--, b--;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+    for (int i = 0; i < n; i++)
+        if (v[i] != 1) dist[i][v[i]] = 1;
+    debug(dist);
+    dfs(g, v, 0, -1);
+    cout << ans << endl;
+
+
+#ifdef LOCAL
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s";
+#endif
+
+    return 0;
+}
