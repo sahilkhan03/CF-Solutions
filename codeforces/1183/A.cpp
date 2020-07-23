915d7d448b7c33871ce4eaca837e552d86353ed7
Sahil <sahilkhan0307.sk@gmail.com>
2020-07-23 18:54:00 +0530

    Add solution for problem `A - Nearest Interesting Number` Link: https://codeforces.com/contest/1183/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1183/submission/87776388

---
+++ codeforces/1183/A.cpp
@@ -0,0 +1,100 @@
+
+// Problem : A. Nearest Interesting Number
+// Contest : Codeforces - Codeforces Round #570 (Div. 3)
+// URL : https://codeforces.com/contest/1183/problem/A
+// Memory Limit : 256 MB
+// Time Limit : 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+/*
+    Author : DemonStar
+*/
+#pragma GCC optimize("Ofast")
+#pragma GCC target ("sse4")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto &i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+const ll mod = 1e9 + 7;
+
+ll calc(ll n) {
+    ll sum = 0;
+    while (n) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main()
+{
+    fast;
+    ll n;
+    cin >> n;
+    while (calc(n) % 4 != 0) n++;
+    cout << n << endl;
+ 
+
+
+#ifdef LOCAL
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s";
+#endif
+
+    return 0;
+}
