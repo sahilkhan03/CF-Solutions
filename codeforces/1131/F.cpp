bba6dc5ea8032a6f2da53e8da456740c3b2464a7
Sahil <sahilkhan0307.sk@gmail.com>
2020-07-24 13:43:00 +0530

    Add solution for problem `F - Asya And Kittens` Link: https://codeforces.com/contest/1131/problem/F Tags: constructive algorithms, dsu, *1700 Ref: https://codeforces.com/contest/1131/submission/87841162

---
+++ codeforces/1131/F.cpp
@@ -0,0 +1,108 @@
+
+// Problem : F. Asya And Kittens
+// Contest : Codeforces - Codeforces Round #541 (Div. 2)
+// URL : https://codeforces.com/contest/1131/problem/F
+// Memory Limit : 256 MB
+// Time Limit : 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target ("sse4")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto &i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+const ll mod = 1e9 + 7;
+
+vector<vl> v;
+vl link, sz;
+
+ll find(ll a) {
+	while(a != link[a]) a = link[a];
+	return a;
+} 
+void unite(ll a, ll b) {
+	a = find(a), b = find(b);
+	if(sz[a] > sz[b]) swap(a, b);
+	sz[b] += sz[a];
+	link[a] = b;
+	for(auto x: v[a]) v[b].push_back(x);	
+}
+
+int main()
+{
+    fast;
+	ll n;
+	cin >> n;
+	v.resize(n);
+	link.resize(n);
+	sz.resize(n, 1);
+	for(int i = 0; i < n; i++) link[i] = i, v[i].pb(i);
+	for(int i = 0; i < n - 1; i++) {
+		ll a, b;
+		cin >> a >> b;
+		a--,b--;
+		unite(a, b);
+	}
+	ll p = find(0);
+	for(auto x: v[p]) cout << x + 1 << " ";
+
+    return 0;
+}
