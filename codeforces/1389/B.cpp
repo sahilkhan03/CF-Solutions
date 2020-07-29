e32712ae8ee47c7b0a7ec118979e7fee9ad1af4f
Sahil <sahilkhan0307.sk@gmail.com>
2020-07-29 21:38:00 +0530

    Add solution for problem `B - Array Walk` Link: https://codeforces.com/contest/1389/problem/B Tags: brute force, dp, greedy, *1600 Ref: https://codeforces.com/contest/1389/submission/88352311

---
+++ codeforces/1389/B.cpp
@@ -0,0 +1,105 @@
+
+// Problem : B. Array Walk
+// Contest : Codeforces - Educational Codeforces Round 92 (Rated for Div. 2)
+// URL : https://codeforces.com/contest/1389/problem/B
+// Memory Limit : 256 MB
+// Time Limit : 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+/*
+	“Whether you think you can, or you think you can’t – you’re right” - Henry Ford 
+*/
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target ("sse4")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto &i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+const ll mod = 1e9 + 7;
+
+int main()
+{
+	fast;
+   ll t;
+   cin >> t;
+   while(t--) {
+   	ll n, k, z;
+   	cin >> n >> k >> z;
+   	vl v(n);
+   	cin >> v;
+   	vl pre(n + 1, 0); 
+   	for(int i = 0; i < n; i++) pre[i + 1] = pre[i] + v[i];
+   	vl p(n, 0);
+   		for(int j = 1; j < n; j++) {
+   			p[j] = p[j - 1];
+   			p[j] = max(p[j], pre[j + 1] - pre[j - 1]);
+   		}
+   	debug(p);
+   	ll ans = pre[k + 1];
+   	for(int i = 1; i <= z; i++) {
+   		if(k - 2*i + 1 >= 0)
+   		ans = max(ans, pre[k - 2*i + 1] + i * p[k - 2*i + 1]);
+   	}
+   	cout << ans << endl;
+   }
+
+	return 0;
+}
