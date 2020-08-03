5863c8194a95cc08b9ccd452bd824fba4596590d
sahil <sahilkhan0307.sk@gmail.com>
2020-08-03 11:02:00 +0530

    Add solution for problem `E - Logs` Link: https://atcoder.jp/contests/abc174/tasks/abc174_e Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc174/submissions/15659274

---
+++ atcoder/abc174/E.cpp
@@ -0,0 +1,107 @@
+
+// Problem : E - Logs
+// Contest : AtCoder - AtCoder Beginner Contest 174
+// URL : https://atcoder.jp/contests/abc174/tasks/abc174_e
+// Memory Limit : 1024 MB
+// Time Limit : 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+/*
+	“Whether you think you can, or you think you can’t – you’re right” - Henry Ford 
+*/
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target ("sse4")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto &i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+const ll mod = 1e9 + 7;
+
+ll check(vl &v, ll mid) {
+	ll c = 0;
+	for(int i = 0; i < v.size(); i++) {
+		c += (v[i] + mid - 1) / mid - 1;
+	}
+	return c;
+}
+
+void solve() {
+	ll n, k; cin >> n >> k;
+	vl v(n); cin >> v;
+	ll lo = 1, hi = mod, ans = hi, mid;
+	while(lo <= hi) {
+		mid = lo + (hi - lo) / 2;
+		if(check(v, mid) <= k) ans = mid, hi = mid - 1;
+		else lo = mid + 1;
+	}
+	cout << ans << endl;
+}
+
+int main()
+{
+	fast;
+   ll t = 1;
+   // cin >> t;
+   while(t--) {
+   	solve();
+   }
+	return 0;
+}
