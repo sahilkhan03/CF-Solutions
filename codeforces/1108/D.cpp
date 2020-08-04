bb39b02494a5f686686fd865b013c883c858e6e8
Sahil <sahilkhan0307.sk@gmail.com>
2020-08-04 12:03:00 +0530

    Add solution for problem `D - Diverse Garland` Link: https://codeforces.com/contest/1108/problem/D Tags: constructive algorithms, dp, greedy, *1400 Ref: https://codeforces.com/contest/1108/submission/88846301

---
+++ codeforces/1108/D.cpp
@@ -0,0 +1,121 @@
+
+// Problem : D. Diverse Garland
+// Contest : Codeforces - Codeforces Round #535 (Div. 3)
+// URL : https://codeforces.com/contest/1108/problem/D
+// Memory Limit : 256 MB
+// Time Limit : 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+/*
+	“Whether you think you can, or you think you can’t – you’re right” - Henry Ford 
+*/
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto &i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+const ll mod = 1e9 + 7;
+
+void solve() {
+	ll n; cin >> n;
+	string s; cin >> s;
+	vector<vl> dp(3, vl(n + 1, 0));
+	vector<vector<char>> pt(3, vector<char>(n + 1, '.'));
+	for(int i = 1; i <= n; i++) {
+		dp[0][i] = min(dp[1][i - 1], dp[2][i - 1]) + (s[i - 1] != 'R');
+		dp[1][i] = min(dp[0][i - 1], dp[2][i - 1]) + (s[i - 1] != 'G');
+		dp[2][i] = min(dp[1][i - 1], dp[0][i - 1]) + (s[i - 1] != 'B');
+		if(dp[1][i - 1] <= dp[2][i - 1]) pt[0][i] = 'G';
+		else pt[0][i] = 'B';
+		if(dp[0][i - 1] <= dp[2][i - 1]) pt[1][i] = 'R';
+		else pt[1][i] = 'B';
+		if(dp[1][i - 1] <= dp[0][i - 1]) pt[2][i] = 'G';
+		else pt[2][i] = 'R';
+	}
+	ll c = min({dp[0][n], dp[1][n], dp[2][n]});
+	string ans = "";
+	ll p = 0;
+	if(c == dp[0][n]) {
+		ans += 'R'; p = 0;
+	}
+	else if(c == dp[1][n]) ans += 'G', p = 1;
+	else ans += 'B', p = 2;
+	for(int i = n; i > 0; i--) {
+		ans += pt[p][i];
+		if(pt[p][i] == 'R') p = 0;
+		else if(pt[p][i] == 'G') p = 1;
+		else p = 2;
+	}
+	ans.pop_back();
+	reverse(all(ans));
+	cout << c << endl << ans << endl;
+}
+
+int main()
+{
+	fast;
+   ll t = 1;
+   // cin >> t;
+   while(t--) {
+   	solve();
+   }
+	return 0;
+}
