107562ee8c0ab053b7fde9762dca16146a13beb1
Sahil <sahilkhan0307.sk@gmail.com>
2020-08-09 20:57:00 +0530

    Add solution for problem `C - Cyclic Permutations ` Link: https://codeforces.com/contest/1391/problem/C Tags: combinatorics, dp, graphs, math, *1500 Ref: https://codeforces.com/contest/1391/submission/89437915

---
+++ codeforces/1391/C.cpp
@@ -0,0 +1,118 @@
+
+// Problem : C. Cyclic Permutations
+// Contest : Codeforces - Codeforces Round #663 (Div. 2)
+// URL : https://codeforces.com/contest/1391/problem/C
+// Memory Limit : 256 MB
+// Time Limit : 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+/*
+	“Whether you think you can, or you think you can’t – you’re right” - Henry Ford 
+*/
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto &i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+const ll mod = 1e9 + 7;
+vl fact(1e6 + 5), inv(1e6 + 5);
+
+ll modpow(ll n, ll p) {
+	ll ans = 1;
+	while(p) {
+		if(p & 1) (ans *= n) %= mod;
+		(n *= n) %= mod; p /= 2;
+	}
+	return ans;
+}
+
+ll calc(ll n, ll r) {
+	ll ans = fact[n];
+	(ans *= inv[r]) %= mod;
+	(ans *= inv[n - r]) %= mod;
+	return ans;
+}
+
+void solve() {
+	ll n; cin >> n;
+	fact[0] = 1, inv[0] = 1;
+	for(ll i = 1; i <= n; i++) {
+		fact[i] = (fact[i - 1]*i) % mod;
+		inv[i] = modpow(fact[i], mod - 2);
+	}
+	ll ans = fact[n];
+	for(ll i = 1; i < n - 1; i++) {
+		ll cur = calc(n - 1, i);
+		(ans += mod - cur) %= mod;
+	}	
+	cout << (ans + mod - 2) % mod << endl;
+}
+
+int main()
+{
+	fast;
+   ll t = 1;
+   // cin >> t;
+   while(t--) {
+   	solve();
+   }
+	return 0;
+}
