ec6c089c2ee95961b951fb39217140dbc0c66105
Sahil <sahilkhan0307.sk@gmail.com>
2020-08-11 12:43:00 +0530

    Add solution for problem `D - Cutting Out` Link: https://codeforces.com/contest/1077/problem/D Tags: binary search, sortings, *1600 Ref: https://codeforces.com/contest/1077/submission/89556464

---
+++ codeforces/1077/D.cpp
@@ -0,0 +1,113 @@
+
+// Problem : D. Cutting Out
+// Contest : Codeforces - Codeforces Round #521 (Div. 3)
+// URL : https://codeforces.com/contest/1077/problem/D
+// Memory Limit : 256 MB
+// Time Limit : 3000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+/*
+	“Whether you think you can, or you think you can’t – you’re right” - Henry Ford 
+*/
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto &i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+const ll mod = 1e9 + 7;
+
+void solve() {
+	ll n, k;
+	cin >> n >> k;
+	vl v(n); cin >> v;
+	vl m(2e5 + 5, 0);
+	for(auto x: v) m[x]++;
+	vector<pl> sz;
+	for(int i = 1; i <= 2e5; i++) sz.pb({m[i], i});
+	sort(all(sz), greater<pl>());
+	ll lo = 1, hi = 2e5 + 5, ans = 1;
+	while(lo <= hi) {
+		ll mid = (lo + hi) /2;
+		ll cnt = 0;
+		for(auto x: sz) cnt += x.F/mid;
+		if(cnt >= k) ans = mid, lo = mid + 1;
+		else hi = mid - 1;
+	}
+	ll s = 0, i = 0;
+	for(auto x: sz) {
+		ll c = x.F/ans;
+		while(c-- and s < k) {
+			cout << x.S << " ";
+			s++;
+		}
+	}
+}
+
+int main()
+{
+	fast;
+   ll t = 1;
+   // cin >> t;
+   while(t--) {
+   	solve();
+   }
+	return 0;
+}
