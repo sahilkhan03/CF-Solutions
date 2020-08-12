f08e7e120d570f61a9f2586ac1cba338687bd002
Sahil <sahilkhan0307.sk@gmail.com>
2020-08-12 18:10:00 +0530

    Add solution for problem `C - Bear and Prime Numbers` Link: https://codeforces.com/contest/385/problem/C Tags: binary search, brute force, data structures, dp, implementation, math, number theory, *1700 Ref: https://codeforces.com/contest/385/submission/89651188

---
+++ codeforces/385/C.cpp
@@ -0,0 +1,114 @@
+
+// Problem : C. Bear and Prime Numbers
+// Contest : Codeforces - Codeforces Round #226 (Div. 2)
+// URL : https://codeforces.com/contest/385/problem/C
+// Memory Limit : 512 MB
+// Time Limit : 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+/*
+	“Whether you think you can, or you think you can’t – you’re right” - Henry Ford 
+*/
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto &i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+const ll mod = 1e9 + 7;
+vi sieve(1e7 + 1), prime, pre(1e7 + 1);
+
+void solve() {
+	for(int i = 2; i < 1e7 + 1; i++) {
+		if(!sieve[i]) {
+			prime.pb(i);
+			for(int j = i; j <= 1e7; j += i) sieve[j] = i;
+		}
+	}
+	int n; cin >> n;
+	for(int i = 0; i < n; i++) {
+		int a; cin >> a;
+		while(a != 1) {
+			int e = sieve[a];
+			pre[e]++;
+			while(sieve[a] == e) a /= e;
+		}
+	}
+	for(int i = 2; i <= 1e7; i++) pre[i] += pre[i - 1];
+	int m; cin >> m;
+	while(m--) {
+		int a,  b;
+		cin >> a >> b;
+		a = min(int(1e7), a);
+		b = min(int(1e7), b);
+		cout << pre[b] - pre[a - 1] << endl;
+	}
+}
+
+int main()
+{
+	fast;
+   ll t = 1;
+   // cin >> t;
+   while(t--) {
+   	solve();
+   }
+	return 0;
+}
