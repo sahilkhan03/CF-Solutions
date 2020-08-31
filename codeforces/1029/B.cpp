ebcd2e7da054d4874bb57398c0c6996f079ff7ca
Sahil <sahilkhan0307.sk@gmail.com>
2020-08-31 16:40:00 +0530

    Add solution for problem `B - Creating the Contest` Link: https://codeforces.com/contest/1029/problem/B Tags: dp, greedy, math, *1200 Ref: https://codeforces.com/contest/1029/submission/91484467

---
+++ codeforces/1029/B.cpp
@@ -0,0 +1,61 @@
+/* “Whether you think you can or you think you can’t, you’re right” - Henry Ford */
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto &i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+const ll mod = 1e9 + 7;
+
+void solve() {
+    ll n; cin >> n;
+    vl v(n); cin >> v;
+    ll ans = 1, cur = 1;
+    for(int i = 1; i < n; i++) {
+        if(v[i] > 2*v[i - 1]) cur = 1;
+        else cur++;
+        ans = max(ans, cur);
+    }
+    ans = max(ans, cur);
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
