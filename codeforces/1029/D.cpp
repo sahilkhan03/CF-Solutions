bedafef6e3da7110b2447dc76725bc45e64a3e53
Sahil <sahilkhan0307.sk@gmail.com>
2020-08-31 17:44:00 +0530

    Add solution for problem `D - Concatenated Multiples` Link: https://codeforces.com/contest/1029/problem/D Tags: implementation, math, *1900 Ref: https://codeforces.com/contest/1029/submission/91489005

--- codeforces/1029/D.cpp
+++ codeforces/1029/D.cpp
@@ -4,10 +4,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-#define fast                      \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define all(x) x.begin(), x.end()
 #define F first
 #define S second
@@ -18,41 +15,31 @@ typedef long long ll;
 #define endl '\n'
 
 template <typename T, typename TT>
-ostream &operator<<(ostream &os, const pair<T, TT> &t)
-{
+ostream &operator<<(ostream &os, const pair<T, TT> &t) {
     return os << t.first << " " << t.second;
 }
 template <typename T>
-ostream &operator<<(ostream &os, const vector<T> &t)
-{
-    for (auto &i : t)
-        os << i << " ";
+ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto &i : t) os << i << " ";
     return os;
 }
 template <typename T>
-istream &operator>>(istream &is, vector<T> &v)
-{
-    for (T &t : v)
-        is >> t;
+istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
     return is;
 }
 template <typename T1, typename T2>
-istream &operator>>(istream &is, vector<pair<T1, T2>> &v)
-{
-    for (pair<T1, T2> &t : v)
-        is >> t.first >> t.second;
+istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
     return is;
 }
 
 const ll mod = 1e9 + 7;
 
-ll modpow(ll n, ll p, ll k)
-{
+ll modpow(ll n, ll p, ll k) {
     ll ans = 1;
-    while (p)
-    {
-        if (p & 1)
-            (ans *= n) %= k;
+    while(p) {
+        if(p & 1) (ans *= n) %= k;
         (n *= n) %= k, p /= 2;
     }
     return ans;
@@ -74,32 +61,24 @@ struct custom_hash
     }
 };
 
-void solve()
-{
+void solve() {
     ll n, k;
     cin >> n >> k;
-    vl v(n);
-    cin >> v;
+    vl v(n); cin >> v;
     ll ans = 0;
-    vector<map<ll, ll>> m(11);
-    for (int i = 0; i < n; i++)
-    {
+    vector<unordered_map<ll, ll, custom_hash>> m(11);
+    for(int i = 0; i < n; i++) {
         ll len = log10(v[i]) + 1;
-        m[len][v[i] % k]++;
+        m[len][v[i]%k]++;
     }
     vl mdpw(12);
-    for (int i = 0; i < 12; i++)
-        mdpw[i] = modpow(10, i, k);
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 1; j < 11; j++)
-        {
-            ll x = v[i] % k;
+    for(int i = 0; i < 12; i++) mdpw[i] = modpow(10, i, k);
+    for(int i = 0; i < n; i++) {
+        for(int j = 1; j < 11; j++) {
+            ll x = v[i]%k;
             (x *= mdpw[j]) %= k;
             ll y = (k - x) % k;
-            if(m[j].find(y) != m[j].end()) {
-                ans += m[j][y] - ((j == (ll)log10(v[i]) + 1) and (y == v[i] % k));
-            }
+            ans += m[j][y] - ((j == (ll)log10(v[i]) + 1) and (y == v[i]%k));
         }
     }
     cout << ans << endl;
@@ -110,8 +89,7 @@ int main()
     fast;
     ll t = 1;
     // cin >> t;
-    while (t--)
-    {
+    while (t--) {
         solve();
     }
     return 0;
