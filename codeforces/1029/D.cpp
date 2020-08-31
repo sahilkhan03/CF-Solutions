86ac6ed7a93fa98f5d6c56be936346b8d39f2707
Sahil <sahilkhan0307.sk@gmail.com>
2020-08-31 17:38:00 +0530

    Add solution for problem `D - Concatenated Multiples` Link: https://codeforces.com/contest/1029/problem/D Tags: implementation, math, *1900 Ref: https://codeforces.com/contest/1029/submission/91488534

--- codeforces/1029/D.cpp
+++ codeforces/1029/D.cpp
@@ -45,28 +45,12 @@ ll modpow(ll n, ll p, ll k) {
     return ans;
 }
 
-struct custom_hash
-{
-    static uint64_t splitmix64(uint64_t x)
-    {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-    size_t operator()(uint64_t x) const
-    {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
-
 void solve() {
     ll n, k;
     cin >> n >> k;
     vl v(n); cin >> v;
     ll ans = 0;
-    vector<unordered_map<ll, ll, custom_hash>> m(11);
+    vector<map<ll, ll>> m(11);
     for(int i = 0; i < n; i++) {
         ll len = log10(v[i]) + 1;
         m[len][v[i]%k]++;
