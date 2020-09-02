c1ca68fd8642b73111d8c146eacaa21fa6ec9c1f
Sahil <sahilkhan0307.sk@gmail.com>
2020-09-02 20:27:00 +0530

    Add solution for problem `D - Two Strings Swaps` Link: https://codeforces.com/contest/1006/problem/D Tags: implementation, *1700 Ref: https://codeforces.com/contest/1006/submission/91651840

---
+++ codeforces/1006/D.cpp
@@ -0,0 +1,106 @@
+/*
+    "Whether you think you can or you think you can't, you're right"
+        - Henry Ford
+*/
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (T i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const set<T> &t) {
+    for (auto i : t) os << i << " ";
+    return os;
+}
+template <typename T1, typename T2>
+inline ostream &operator<<(ostream &os, const map<T1, T2> &t) {
+    for (auto &i : t) os << i.first << " : " << i.second << endl;
+    return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+    bool first;
+    string separator;
+    Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+    template <typename ObjectType>
+    Debugger &operator, (const ObjectType &v) {
+        if (!first) cerr << separator;
+        cerr << v;
+        first = false;
+        return *this;
+    }
+    ~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+const ll mod = 1e9 + 7;
+
+void solve() {
+    ll n; cin >> n;
+    string a, b;
+    cin >> a >> b;
+    ll ans = 0;
+    for (int i = 0; i < n / 2; i++) {
+        vector<char> v = {a[i], b[i], a[n - i - 1], b[n - 1 - i]};
+        set<char> s(all(v));
+        if (s.size() == 1) continue;
+        if (s.size() == 4) {
+            ans += 2;
+            continue;
+        }
+        if (s.size() == 2) {
+            if (v[0] == v[1] and v[2] != v[3]) ans++;
+            else if (v[2] == v[3] and v[0] != v[1]) ans++;
+            continue;
+        }
+        if (v[0] == v[2]) ans += 2;
+        else ans++;
+    }
+    if (n & 1) ans += (a[n / 2] != b[n / 2]);
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
