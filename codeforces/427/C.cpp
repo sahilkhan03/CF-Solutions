125fe7bf53116e8766be9051b9113033f7fb224c
Sahil <sahilkhan0307.sk@gmail.com>
2020-09-13 20:19:00 +0530

    Add solution for problem `C - Checkposts` Link: https://codeforces.com/contest/427/problem/C Tags: dfs and similar, graphs, two pointers, *1700 Ref: https://codeforces.com/contest/427/submission/92723962

--- codeforces/427/C.cpp
+++ codeforces/427/C.cpp
@@ -1,5 +1,5 @@
 /*
-    "Whether you think you can, or you think you can't, you're right"
+    "Whether you think you can or you think you can't, you're right"
         - Henry Ford
 */
 
@@ -23,17 +23,17 @@ inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
 }
 template <typename T>
 inline ostream &operator<<(ostream &os, const vector<T> &t) {
-    for (T i : t) os << i << " ";
+    for (auto i : t) os << i << " ";
     return os;
 }
 template <typename T>
 inline ostream &operator<<(ostream &os, const set<T> &t) {
-    for (T &i : t) os << i << " ";
+    for (auto i : t) os << i << " ";
     return os;
 }
 template <typename T1, typename T2>
 inline ostream &operator<<(ostream &os, const map<T1, T2> &t) {
-    for (auto &i : t) os << i.first << " : " << i.second << endl;
+    for (auto i : t) os << i.first << " : " << i.second << endl;
     return os;
 }
 template <typename T>
@@ -68,112 +68,52 @@ public:
 #endif
 
 const ll mod = 1e9 + 7;
-template <class E> struct csr {
-    std::vector<int> start;
-    std::vector<E> elist;
-    csr(int n, const std::vector<std::pair<int, E>>& edges)
-        : start(n + 1), elist(edges.size()) {
-        for (auto e : edges) {
-            start[e.first + 1]++;
-        }
-        for (int i = 1; i <= n; i++) {
-            start[i] += start[i - 1];
-        }
-        auto counter = start;
-        for (auto e : edges) {
-            elist[counter[e.first]++] = e.second;
-        }
+
+ll n, idx = 0;
+vector<vl> v, inv;
+vector<bool> onstc;
+stack<ll> s;
+vl id, low, cost;
+pl ans = {0, 1}; // {cost, ways}
+
+void dfs(ll u) {
+    id[u] = low[u] = idx++;
+    onstc[u] = 1; s.push(u);
+    for (auto x : v[u]) {
+        if (id[x] == -1) dfs(x);
+        if (onstc[x]) low[u] = min(low[u], low[x]);
     }
-};
-struct scc_graph {
-public:
-    scc_graph(int n) : _n(n) {}
-
-    int num_vertices() { return _n; }
-
-    void add_edge(int from, int to) { edges.push_back({from, {to}}); }
-
-    std::pair<int, std::vector<int>> scc_ids() {
-        auto g = csr<edge>(_n, edges);
-        int now_ord = 0, group_num = 0;
-        std::vector<int> visited, low(_n), ord(_n, -1), ids(_n);
-        visited.reserve(_n);
-        auto dfs = [&](auto self, int v) -> void {
-            low[v] = ord[v] = now_ord++;
-            visited.push_back(v);
-            for (int i = g.start[v]; i < g.start[v + 1]; i++) {
-                auto to = g.elist[i].to;
-                if (ord[to] == -1) {
-                    self(self, to);
-                    low[v] = std::min(low[v], low[to]);
-                } else {
-                    low[v] = std::min(low[v], ord[to]);
-                }
-            }
-            if (low[v] == ord[v]) {
-                while (true) {
-                    int u = visited.back();
-                    visited.pop_back();
-                    ord[u] = _n;
-                    ids[u] = group_num;
-                    if (u == v) break;
-                }
-                group_num++;
-            }
-        };
-        for (int i = 0; i < _n; i++) {
-            if (ord[i] == -1) dfs(dfs, i);
+    if (id[u] == low[u]) {
+        pl c = {1e18, 0}; // {cost, ways} for current SCC
+        vl scc;
+        while (true) {
+            ll w = s.top(); s.pop();
+            scc.pb(w);
+            onstc[w] = 0;
+            if (w == u) break;
         }
-        for (auto& x : ids) {
-            x = group_num - 1 - x;
+        for (auto x : scc) {
+            if (cost[x] < c.F) c = {cost[x], 1};
+            else if (cost[x] == c.F) c.S++;
         }
-        return {group_num, ids};
+        ans.F += c.F;
+        (ans.S *= c.S) %= mod;
     }
-
-    std::vector<std::vector<int>> scc() {
-        auto ids = scc_ids();
-        int group_num = ids.first;
-        std::vector<int> counts(group_num);
-        for (auto x : ids.second) counts[x]++;
-        std::vector<std::vector<int>> groups(ids.first);
-        for (int i = 0; i < group_num; i++) {
-            groups[i].reserve(counts[i]);
-        }
-        for (int i = 0; i < _n; i++) {
-            groups[ids.second[i]].push_back(i);
-        }
-        return groups;
-    }
-
-private:
-    int _n;
-    struct edge {
-        int to;
-    };
-    std::vector<std::pair<int, edge>> edges;
-};
+}
 
 void solve() {
-    ll n; cin >> n;
-    vl cost(n); cin >> cost;
-    scc_graph v(n);
+    cin >> n;
+    cost.resize(n); cin >> cost;
+    v.resize(n); id.resize(n, -1), low.resize(n, -1);
     ll m; cin >> m;
     while (m--) {
         ll a, b;
         cin >> a >> b;
-        a--, b--;
-        v.add_edge(a, b);
+        v[a - 1].pb(b - 1);
     }
-    pl ans = {0, 1};
-    auto res = v.scc();
-    for (auto& x : res) {
-        pl cur = {1e18, 0};
-        for (auto y : x) {
-            if (cost[y] < cur.F) cur = {cost[y], 1};
-            else if (cost[y] == cur.F) cur.S++;
-        }
-        ans.F += cur.F;
-        (ans.S *= cur.S) %= mod;
+    onstc = vector<bool>(n, 0);
+    for (int i = 0; i < n; i++) {
+        if (id[i] == -1) dfs(i);
     }
     cout << ans << endl;
 }
