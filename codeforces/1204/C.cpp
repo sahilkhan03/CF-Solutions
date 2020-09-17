e848c16295d1addd083f9802f70b9a4f8ff82ee7
Sahil <sahilkhan0307.sk@gmail.com>
2020-09-17 12:13:00 +0530

    Add solution for problem `C - Anna, Svyatoslav and Maps` Link: https://codeforces.com/contest/1204/problem/C Tags: dp, graphs, greedy, shortest paths, *1700 Ref: https://codeforces.com/contest/1204/submission/93023650

---
+++ codeforces/1204/C.cpp
@@ -0,0 +1,112 @@
+/*
+    "Whether you think you can or you think you can't, you're right"
+        - Henry Ford
+*/
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const set<T> &t) {
+    for (auto i : t) os << i << " ";
+    return os;
+}
+template <typename T1, typename T2>
+inline ostream &operator<<(ostream &os, const map<T1, T2> &t) {
+    for (auto i : t) os << i.first << " : " << i.second << endl;
+    return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+    bool first;
+    string separator;
+    Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+    template <typename ObjectType>
+    Debugger &operator, (const ObjectType &v) {
+        if (!first) cerr << separator;
+        cerr << v;
+        first = false;
+        return *this;
+    }
+    ~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+const ll mod = 1e9 + 7;
+
+void solve() {
+    ll n; cin >> n;
+    vector<string> s(n);
+    cin >> s;
+    vector<vl> path(n, vl(n, 1e18));
+    for (int i = 0; i < n; i++) {
+        path[i][i] = 0;
+        for (int j = 0; j < n; j++) {
+            if (s[i][j] == '1') path[i][j] = 1;
+        }
+    }
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                path[i][j] = min(path[i][j], path[i][k] + path[k][j]);
+            }
+        }
+    }
+    debug(path);
+    ll m, ls = 0; cin >> m;
+    vl v(m); cin >> v;
+    vl ans = {v[0]};
+    for (int i = 1; i < m - 1; i++) {
+        if (path[ans.back() - 1][v[i + 1] - 1] == i + 1 - ls) continue;
+        ans.pb(v[i]);
+        ls = i;
+    }
+    ans.pb(v.back());
+    cout << ans.size() << endl << ans << endl;
+}
+
+int main()
+{
+    fast;
+    ll T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
