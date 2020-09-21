46656c4997bfa68f7764f91d212c47518518cd60
Sahil <sahilkhan0307.sk@gmail.com>
2020-09-21 10:40:00 +0530

    Add solution for problem `A - Writing Code` Link: https://codeforces.com/contest/543/problem/A Tags: dp, *1800 Ref: https://codeforces.com/contest/543/submission/93377813

---
+++ codeforces/543/A.cpp
@@ -0,0 +1,99 @@
+/*
+    "Whether you think you can or you think you can't, you're right"
+        - Henry Ford
+*/
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+    return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+    for (auto i : t) os << i << " ";
+    return os;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const set<T> &t) {
+    for (auto i : t) os << i << " ";
+    return os;
+}
+template <typename T1, typename T2>
+inline ostream &operator<<(ostream &os, const map<T1, T2> &t) {
+    for (auto i : t) os << i.first << " : " << i.second << endl;
+    return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+    for (T &t : v) is >> t;
+    return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+    for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+    return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+    bool first;
+    string separator;
+    Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+    template <typename ObjectType>
+    Debugger &operator, (const ObjectType &v) {
+        if (!first) cerr << separator;
+        cerr << v;
+        first = false;
+        return *this;
+    }
+    ~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+void solve() {
+    ll n, m, b, mod;
+    cin >> n >> m >> b >> mod;
+    vl cost(n); cin >> cost;
+    vector<vl> dp(m + 1, vl(b + 1, 0));
+    dp[0][0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            for (int k = 0; k <= b; k++) {
+                if (k - cost[i - 1] >= 0)
+                    (dp[j][k] += dp[j - 1][k - cost[i - 1]]) %= mod;
+            }
+        }
+    }
+    ll ans = 0;
+    for (int i = 0; i <= b; i++)
+        (ans += dp[m][i]) %= mod;
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast;
+    ll T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
