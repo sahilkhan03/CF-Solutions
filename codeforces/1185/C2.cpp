b71098d1b29e0c75416be6950f7db74d9ae3982c
Sahil <sahilkhan0307.sk@gmail.com>
2020-09-21 15:03:00 +0530

    Add solution for problem `C2 - Exam in BerSU (hard version)` Link: https://codeforces.com/contest/1185/problem/C2 Tags: brute force, data structures, greedy, math, *1700 Ref: https://codeforces.com/contest/1185/submission/93392284

--- codeforces/1185/C2.cpp
+++ codeforces/1185/C2.cpp
@@ -69,20 +69,63 @@ public:
 
 const ll mod = 1e9 + 7;
 
+template <class T> struct fenwick_tree {
+public:
+    fenwick_tree() : _n(0) {}
+    fenwick_tree(int n) : _n(n), data(n) {}
+
+    void add(int p, T x) {
+        assert(0 <= p && p < _n);
+        p++;
+        while (p <= _n) {
+            data[p - 1] += T(x);
+            p += p & -p;
+        }
+    }
+
+    T sum(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        return sum(r) - sum(l);
+    }
+
+private:
+    int _n;
+    std::vector<T> data;
+
+    T sum(int r) {
+        T s = 0;
+        while (r > 0) {
+            s += data[r - 1];
+            r -= r & -r;
+        }
+        return s;
+    }
+};
+fenwick_tree<ll> fen1(2e5 + 5), fen2(2e5 + 5);
+
+ll calc(ll num) {
+    ll ans = 0, lo = 1, hi = 2e5  + 3;
+    while (lo <= hi) {
+        ll mid = lo + (hi - lo) / 2;
+        if (fen1.sum(1, mid) <= num) ans = mid, lo = mid + 1;
+        else hi = mid - 1;
+    }
+    return ans;
+}
+
 void solve() {
     ll n, m;
     cin >> n >> m;
     vl v(n); cin >> v;
-    vl cnt(101, 0);
+    vector<vl> idx;
+    for (int i = 0; i < n; i++) idx.pb({v[i], i});
+    sort(all(idx));
+    map<pl, ll> inv;
+    for (int i = 0; i < n; i++) inv[ {idx[i][0], idx[i][1]}] = i + 1;
     for (int i = 0; i < n; i++) {
-        ll s = m - v[i], c = 0;
-        for (int j = 1; j < 101; j++) {
-            ll x = min(cnt[j], s / j);
-            c += x;
-            s -= x * j;
-        }
-        cout << i - c << " ";
-        cnt[v[i]]++;
+        cout << i - fen2.sum(1, calc(m - v[i])) << " ";
+        fen1.add(inv[ {v[i], i}], v[i]);
+        fen2.add(inv[ {v[i], i}], 1);
     }
 }
 
