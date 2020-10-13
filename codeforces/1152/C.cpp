b6e5f67f81eda2667c9ee5c1e71744909d17406e
Sahil <sahilkhan0307.sk@gmail.com>
2020-10-13 13:34:00 +0530

    Add solution for problem `C - Neko does Maths` Link: https://codeforces.com/contest/1152/problem/C Tags: brute force, math, number theory, *1800 Ref: https://codeforces.com/contest/1152/submission/95402850

---
+++ codeforces/1152/C.cpp
@@ -0,0 +1,95 @@
+/*
+	"Work until your Idols become your Rivals"
+*/
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+	return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const set<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T1, typename T2>
+inline ostream &operator<<(ostream &os, const map<T1, T2> &t) {
+	for (auto i : t) os << i.first << " : " << i.second << endl;
+	return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+	for (T &t : v) is >> t;
+	return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+	for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+	return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+	bool first;
+	string separator;
+	Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+	template <typename ObjectType>
+	Debugger &operator, (const ObjectType &v) {
+		if (!first) cerr << separator;
+		cerr << v;
+		first = false;
+		return *this;
+	}
+	~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+const ll mod = 1e9 + 7;
+
+void solve() {
+	ll a, b;
+	cin >> a >> b;
+	ll n = max(a, b) - min(a, b);
+	ll lcm = (a * b) / __gcd(a, b), ans = 0;
+	for (ll i = 1; i * i <= n; i++) {
+		if (n % i == 0) {
+			ll k1 = i - a % i, k2 = n / i - a % (n / i);
+			if ((a + k1) * (b + k1) / i < lcm) lcm = (a + k1) * (b + k1) / i, ans = k1;
+			if ((a + k2) * (b + k2) / (n / i) < lcm) lcm = (a + k2) * (b + k2) / (n / i), ans = k2;
+		}
+	}
+	cout << ans << endl;
+}
+
+int main()
+{
+	fast;
+	ll T = 1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+	return 0;
+}
