51ca028058c56935facd8028fe3b20091ecac718
Sahil <sahilkhan0307.sk@gmail.com>
2020-10-13 20:09:00 +0530

    Add solution for problem `C - Painting Fence` Link: https://codeforces.com/contest/448/problem/C Tags: divide and conquer, dp, greedy, *1900 Ref: https://codeforces.com/contest/448/submission/95436178

--- codeforces/448/C.cpp
+++ codeforces/448/C.cpp
@@ -65,62 +65,14 @@ public:
 #else
 #define debug(args...)
 #endif
-template<typename T> struct RMQ {
-	vector<T> v; int n;
-	static const int b = 30;
-	vector<int> mask, t;
 
-	int op(int x, int y) {
-		return v[x] < v[y] ? x : y;
-	}
-
-	int lsb(int x) {
-		return x & -x;
-	}
-
-	int msb_index(int x) {
-		return __builtin_clz(1) - __builtin_clz(x);
-	}
-
-	int small(int r, int size = b) {
-		int dist_from_r = msb_index(mask[r] & ((1 << size) - 1));
-		return r - dist_from_r;
-	}
-
-	RMQ(const vector<T>& v_) : v(v_), n(v.size()), mask(n), t(n) {
-		int curr_mask = 0;
-		for (int i = 0; i < n; i++) {
-			curr_mask = (curr_mask << 1) & ((1 << b) - 1);
-			while (curr_mask > 0 and op(i, i - msb_index(lsb(curr_mask))) == i) {
-				curr_mask ^= lsb(curr_mask);
-			}
-			curr_mask |= 1;
-
-			mask[i] = curr_mask;
-		}
-
-		for (int i = 0; i < n / b; i++) t[i] = small(b * i + b - 1);
-		for (int j = 1; (1 << j) <= n / b; j++) for (int i = 0; i + (1 << j) <= n / b; i++)
-				t[n / b * j + i] = op(t[n / b * (j - 1) + i], t[n / b * (j - 1) + i + (1 << (j - 1))]);
-	}
-	T query(int l, int r) {
-		if (r - l + 1 <= b) return v[small(r, r - l + 1)];
-		int ans = op(small(l + b - 1), small(r));
-		int x = l / b + 1, y = r / b - 1;
-		if (x <= y) {
-			int j = msb_index(y - x + 1);
-			ans = op(ans, op(t[n / b * j + x], t[n / b * j + y - (1 << j) + 1]));
-		}
-		return v[ans];
-	}
-};
 const ll mod = 1e9 + 7;
 ll n;
 vl v;
-RMQ<ll> rm(vl({1}));
+
 ll calc(ll l, ll r, ll h) {
 	if (l > r) return 0;
-	ll mn = rm.query(l, r);
+	ll mn = *min_element(v.begin() + l, v.begin() + r + 1);
 	ll ls = l, ans = mn - h;
 	for (int i = l; i <= r; i++) {
 		if (v[i] == mn) {
@@ -135,7 +87,6 @@ ll calc(ll l, ll r, ll h) {
 void solve() {
 	cin >> n;
 	v = vl(n); cin >> v;
-	rm = RMQ(v);
 	cout << calc(0, n - 1, 0) << endl;
 }
 
