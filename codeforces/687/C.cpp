8cd7fde1c8e626f65e6d71e60fe8a245b6ed8791
Sahil <sahilkhan0307.sk@gmail.com>
2020-10-15 13:07:00 +0530

    Add solution for problem `C - The Values You Can Make` Link: https://codeforces.com/contest/687/problem/C Tags: dp, *1900 Ref: https://codeforces.com/contest/687/submission/95564102

---
+++ codeforces/687/C.cpp
@@ -0,0 +1,95 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+	return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const set<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T1, typename T2>
+inline ostream &operator<<(ostream &os, const map<T1, T2> &t) {
+	for (auto i : t) os << i.first << " : " << i.second << endl;
+	return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+	for (T &t : v) is >> t;
+	return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+	for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+	return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+	bool first;
+	string separator;
+	Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+	template <typename ObjectType>
+	Debugger &operator, (const ObjectType &v) {
+		if (!first) cerr << separator;
+		cerr << v;
+		first = false;
+		return *this;
+	}
+	~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+const ll mod = 1e9 + 7;
+bool dp[501][501][501];
+void solve() {
+	ll n, k;
+	cin >> n >> k;
+	vl v(n); cin >> v;
+	for (int i = 0; i <= n; i++) dp[i][0][0] = 1;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j <= k; j++) {
+			for (int x = 0; x <= j; x++) {
+				dp[i][j][x] = (dp[i][j][x] or dp[i - 1][j][x] or (j - v[i - 1] >= 0 ? dp[i - 1][j - v[i - 1]][x] : 0) or (j - v[i - 1] >= 0 and x - v[i - 1] >= 0 ? dp[i - 1][j - v[i - 1]][x - v[i - 1]] : 0));
+			}
+		}
+	}
+	vl ans;
+	for (int i = 0; i <= k; i++) {
+		if (dp[n][k][i]) ans.pb(i);
+	}
+	cout << ans.size() << endl << ans << endl;
+}
+
+int main()
+{
+	fast;
+	ll T = 1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+	return 0;
+}
