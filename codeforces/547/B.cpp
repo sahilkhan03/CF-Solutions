411341c5c9d8409ad118a0c8b0d7d6cb7c920e35
Sahil <sahilkhan0307.sk@gmail.com>
2020-10-17 14:25:00 +0530

    Add solution for problem `B - Mike and Feet` Link: https://codeforces.com/contest/547/problem/B Tags: binary search, data structures, dp, dsu, *1900 Ref: https://codeforces.com/contest/547/submission/95721499

--- codeforces/547/B.cpp
+++ codeforces/547/B.cpp
@@ -62,26 +62,77 @@ public:
 #define debug(args...)
 #endif
 
+template<typename T> struct RMQ {
+	vector<T> v; int n;
+	static const int b = 30;
+	vector<int> mask, t;
+
+	int op(int x, int y) {
+		return v[x] < v[y] ? x : y;
+	}
+
+	int lsb(int x) {
+		return x & -x;
+	}
+
+	int msb_index(int x) {
+		return __builtin_clz(1) - __builtin_clz(x);
+	}
+
+	int small(int r, int size = b) {
+		int dist_from_r = msb_index(mask[r] & ((1 << size) - 1));
+		return r - dist_from_r;
+	}
+
+	RMQ(const vector<T>& v_) : v(v_), n(v.size()), mask(n), t(n) {
+		int curr_mask = 0;
+		for (int i = 0; i < n; i++) {
+			curr_mask = (curr_mask << 1) & ((1 << b) - 1);
+			while (curr_mask > 0 and op(i, i - msb_index(lsb(curr_mask))) == i) {
+				curr_mask ^= lsb(curr_mask);
+			}
+			curr_mask |= 1;
+
+			mask[i] = curr_mask;
+		}
+
+		for (int i = 0; i < n / b; i++) t[i] = small(b * i + b - 1);
+		for (int j = 1; (1 << j) <= n / b; j++) for (int i = 0; i + (1 << j) <= n / b; i++)
+				t[n / b * j + i] = op(t[n / b * (j - 1) + i], t[n / b * (j - 1) + i + (1 << (j - 1))]);
+	}
+	T query(int l, int r) {
+		if (r - l + 1 <= b) return v[small(r, r - l + 1)];
+		int ans = op(small(l + b - 1), small(r));
+		int x = l / b + 1, y = r / b - 1;
+		if (x <= y) {
+			int j = msb_index(y - x + 1);
+			ans = op(ans, op(t[n / b * j + x], t[n / b * j + y - (1 << j) + 1]));
+		}
+		return v[ans];
+	}
+};
+
 const ll mod = 1e9 + 7;
 
 void solve() {
 	ll n; cin >> n;
-	vl v(n), l(n), r(n), ans(n);
-	cin >> v;
-	stack<ll> s;
+	vl v(n); cin >> v;
+	RMQ<ll> rmq(v);
+	vl ans(n, 0);
 	for (int i = 0; i < n; i++) {
-		while (!s.empty() and v[s.top()] >= v[i]) s.pop();
-		if (!s.empty()) l[i] = s.top();
-		else l[i] = -1;
-		s.push(i);
-	}
-	s = stack<ll>();
-	for (int i = n - 1; i >= 0; i--) {
-		while (!s.empty() and v[s.top()] >= v[i]) s.pop();
-		if (!s.empty()) r[i] = s.top();
-		else r[i] = n;
-		ans[r[i] - l[i] - 2] = max(v[i], ans[r[i] - l[i] - 2]);
-		s.push(i);
+		ll lo = i, hi = n - 1, mid, l = i, r = i;
+		while (lo <= hi) {
+			mid = (lo + hi) / 2;
+			if (rmq.query(i, mid) == v[i]) r = mid, lo = mid + 1;
+			else hi = mid - 1;
+		}
+		lo = 0, hi = i;
+		while (lo <= hi) {
+			mid = (lo + hi) / 2;
+			if (rmq.query(mid, i) == v[i]) l = mid, hi = mid - 1;
+			else lo = mid + 1;
+		}
+		ans[r - l] = max(ans[r - l], v[i]);
 	}
 	for (int i = n - 2; i >= 0; i--) ans[i] = max(ans[i], ans[i + 1]);
 	cout << ans << endl;
