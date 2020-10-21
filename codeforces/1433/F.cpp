bc2e09f9363944d95dd6bdafe7931234126b7c8b
Sahil <sahilkhan0307.sk@gmail.com>
2020-10-21 10:52:00 +0530

    Add solution for problem `F - Zero Remainder Sum ` Link: https://codeforces.com/contest/1433/problem/F Tags: dp, *2100 Ref: https://codeforces.com/contest/1433/submission/96205179

---
+++ codeforces/1433/F.cpp
@@ -0,0 +1,107 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+	return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const set<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T1, typename T2>
+inline ostream &operator<<(ostream &os, const map<T1, T2> &t) {
+	for (auto i : t) os << i.first << " : " << i.second << endl;
+	return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+	for (T &t : v) is >> t;
+	return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+	for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+	return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+	bool first;
+	string separator;
+	Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+	template <typename ObjectType>
+	Debugger &operator, (const ObjectType &v) {
+		if (!first) cerr << separator;
+		cerr << v;
+		first = false;
+		return *this;
+	}
+	~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+int dp[71][71][71];
+
+void solve() {
+	int n, m, k;
+	cin >> n >> m >> k;
+	vector<vi> v(n, vi(m));
+	cin >> v;
+	vector<vector<vector<vi>>> dp(n + 1, vector<vector<vi>> (m + 1, vector<vi> (m / 2 + 3, vi(k, -1e9))));
+	dp[0][0][0][0] = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			for (int use = 0; use <= m / 2; use++) {
+				for (int md = 0; md < k; md++) {
+					if (j == m - 1) {
+						dp[i + 1][0][0][md] = max(dp[i + 1][0][0][md], dp[i][j][use][md]);
+						if (use < m / 2) {
+							dp[i + 1][0][0][(md + v[i][j]) % k] = max(
+							        dp[i + 1][0][0][(md + v[i][j]) % k], dp[i][j][use][md] + v[i][j]);
+						}
+					}
+					else {
+						dp[i][j + 1][use][md] = max(dp[i][j + 1][use][md], dp[i][j][use][md]);
+						if (use < m / 2) {
+							dp[i][j + 1][use + 1][(md + v[i][j]) % k] = max(dp[i][j + 1][use + 1][(md + v[i][j]) % k], dp[i][j][use][md] + v[i][j]);
+						}
+					}
+				}
+			}
+		}
+	}
+	cout << max(0, dp[n][0][0][0]) << endl;
+}
+
+int main()
+{
+	fast;
+	ll T = 1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+	return 0;
+}
