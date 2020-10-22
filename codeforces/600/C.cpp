7683ba2ea141ad3ab1b2d63d13ba10d778c78baa
Sahil <sahilkhan0307.sk@gmail.com>
2020-10-22 13:05:00 +0530

    Add solution for problem `C - Make Palindrome` Link: https://codeforces.com/contest/600/problem/C Tags: constructive algorithms, greedy, strings, *1800 Ref: https://codeforces.com/contest/600/submission/96338127

---
+++ codeforces/600/C.cpp
@@ -0,0 +1,98 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+	return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const set<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T1, typename T2>
+inline ostream &operator<<(ostream &os, const map<T1, T2> &t) {
+	for (auto i : t) os << i.first << " : " << i.second << endl;
+	return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+	for (T &t : v) is >> t;
+	return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+	for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+	return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+	bool first;
+	string separator;
+	Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+	template <typename ObjectType>
+	Debugger &operator, (const ObjectType &v) {
+		if (!first) cerr << separator;
+		cerr << v;
+		first = false;
+		return *this;
+	}
+	~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+const ll mod = 1e9 + 7;
+
+void solve() {
+	string s, ans;
+	cin >> s;
+	int n = s.size();
+	vl cnt(26, 0);
+	vl o;
+	for (int i = 0; i < n; i++) {
+		cnt[s[i] - 'a']++;
+	}
+	for (int i = 0; i < 26; i++) {
+		if (cnt[i] & 1) o.pb(i);
+	}
+	for (int i = 0; i < o.size() / 2; i++) cnt[o[i]]++;
+	for (int i = 0; i < 26; i++) {
+		for (int j = 0; j < cnt[i] / 2; j++) ans += char(i + 'a');
+	}
+	cout << ans;
+	if (o.size() & 1) cout << char(o[o.size() / 2] + 'a');
+	reverse(all(ans));
+	cout << ans << endl;
+}
+
+int main()
+{
+	fast;
+	ll T = 1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+	return 0;
+}
