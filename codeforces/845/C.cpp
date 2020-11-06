dae2172d49cf3b1a27a66635e63631483f646c15
Sahil <sahilkhan0307.sk@gmail.com>
2020-11-06 18:24:00 +0530

    Add solution for problem `C - Two TVs` Link: https://codeforces.com/contest/845/problem/C Tags: data structures, greedy, sortings, *1500 Ref: https://codeforces.com/contest/845/submission/97777767

---
+++ codeforces/845/C.cpp
@@ -0,0 +1,91 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+	return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const set<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T1, typename T2>
+inline ostream &operator<<(ostream &os, const map<T1, T2> &t) {
+	for (auto i : t) os << i.first << " : " << i.second << endl;
+	return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+	for (T &t : v) is >> t;
+	return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+	for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+	return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+	bool first;
+	string separator;
+	Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+	template <typename ObjectType>
+	Debugger &operator, (const ObjectType &v) {
+		if (!first) cerr << separator;
+		cerr << v;
+		first = false;
+		return *this;
+	}
+	~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+const ll mod = 1e9 + 7;
+
+void solve() {
+	ll n; cin >> n;
+	vector<pl> v(n);
+	cin >> v;
+	sort(all(v));
+	multiset<pl> s;
+	ll ans = 0;
+	for (auto x : v) {
+		while (!s.empty() and s.begin()->F < x.F) s.erase(s.begin());
+		s.insert({x.S, x.F});
+		ans = max(ans, (ll)s.size());
+	}
+	cout << (ans > 2 ? "NO" : "YES") << endl;
+}
+
+int main()
+{
+	fast;
+	ll T = 1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+	return 0;
+}
