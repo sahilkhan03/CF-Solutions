3931ec36661e8b2c5facda4863ed3f62fbf4678c
sahil <sahilkhan0307.sk@gmail.com>
2020-11-08 17:42:00 +0530

    Add solution for problem `C - To 3` Link: https://atcoder.jp/contests/abc182/tasks/abc182_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc182/submissions/17960568

---
+++ atcoder/abc182/C.cpp
@@ -0,0 +1,98 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+	return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const set<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T1, typename T2>
+inline ostream &operator<<(ostream &os, const map<T1, T2> &t) {
+	for (auto i : t) os << i.first << " : " << i.second << endl;
+	return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+	for (T &t : v) is >> t;
+	return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+	for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+	return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+	bool first;
+	string separator;
+	Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+	template <typename ObjectType>
+	Debugger &operator, (const ObjectType &v) {
+		if (!first) cerr << separator;
+		cerr << v;
+		first = false;
+		return *this;
+	}
+	~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+const ll mod = 1e9 + 7;
+
+void solve() {
+	ll n; cin >> n;
+	ll d = log10(n) + 1;
+	ll sum = n;
+	vl v(3);
+	while (n) v[(n % 10) % 3]++, n /= 10;
+	if (sum % 3 == 0) {
+		cout << 0 << endl;
+		return;
+	}
+	if (sum % 3 == 1) {
+		if (v[1] and d > 1) cout << 1 << endl;
+		else if (v[2] > 1 and d > 2) cout << 2 << endl;
+		else cout << -1 << endl;
+		return;
+	}
+	if (v[2] and d > 1) cout << 1 << endl;
+	else if (v[1] > 1 and d > 2) cout << 2 << endl;
+	else cout << -1 << endl;
+	return;
+}
+
+int main()
+{
+	fast;
+	ll T = 1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+	return 0;
+}
