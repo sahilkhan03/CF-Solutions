c2bd79e5a52bf58d4df50f40988b3ef89e0c115d
Sahil <sahilkhan0307.sk@gmail.com>
2020-11-11 19:15:00 +0530

    Add solution for problem `D - Paths in a Complete Binary Tree` Link: https://codeforces.com/contest/792/problem/D Tags: bitmasks, trees, *1900 Ref: https://codeforces.com/contest/792/submission/98129727

---
+++ codeforces/792/D.cpp
@@ -0,0 +1,110 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+	return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const set<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T1, typename T2>
+inline ostream &operator<<(ostream &os, const map<T1, T2> &t) {
+	for (auto i : t) os << i.first << " : " << i.second << endl;
+	return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+	for (T &t : v) is >> t;
+	return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+	for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+	return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+	bool first;
+	string separator;
+	Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+	template <typename ObjectType>
+	Debugger &operator, (const ObjectType &v) {
+		if (!first) cerr << separator;
+		cerr << v;
+		first = false;
+		return *this;
+	}
+	~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+const ll mod = 1e9 + 7;
+ll root = 1;
+pl calc(ll u, ll cur, ll sz) {
+	if (u == cur) return {u, sz};
+	if (u > cur) return calc(u, cur + (sz + 1) / 2, sz / 2);
+	return calc(u, cur - (sz + 1) / 2, sz / 2);
+}
+pl calc1(ll u, ll cur, ll sz) {
+	debug('f', cur, sz);
+	if (u == cur + (sz + 1) / 2 or u == cur - (sz + 1) / 2)
+		return {cur, sz};
+	if (u > cur) return calc1(u, cur + (sz + 1) / 2, sz / 2);
+	return calc1(u, cur - (sz + 1) / 2, sz / 2);
+}
+void solve() {
+	ll n, q;
+	cin >> n >> q;
+	root = (n + 1) / 2;
+	while (q--) {
+		ll u; cin >> u;
+		string s; cin >> s;
+		pl cur = calc(u, root, root - 1);
+		debug(cur);
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == 'L') cur.F -= (cur.S + 1) / 2, cur.S /= 2;
+			else if (s[i] == 'R') cur.F += (cur.S + 1) / 2, cur.S /= 2;
+			else if (cur.F != root) {
+				cur = calc1(cur.F, root, root - 1);
+			}
+			debug(cur);
+		}
+		cout << cur.F << endl;
+	}
+}
+
+
+int main()
+{
+	fast;
+	ll T = 1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+	return 0;
+}
