4f2c5af96e4f9eb3f941318adae77c6d21e44e3c
Sahil <sahilkhan0307.sk@gmail.com>
2020-11-16 10:33:00 +0530

    Add solution for problem `D - Catching Cheaters` Link: https://codeforces.com/contest/1447/problem/D Tags: dp, greedy, strings, *1800 Ref: https://codeforces.com/contest/1447/submission/98519353

--- codeforces/1447/D.cpp
+++ codeforces/1447/D.cpp
@@ -74,8 +74,8 @@ void solve() {
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= m; j++) {
 			if (s[i - 1] == t[j - 1])
-				dp[i][j] = max(0, dp[i - 1][j - 1]) + 2;
-			dp[i][j] = max({dp[i][j], dp[i][j - 1] - 1, dp[i - 1][j] - 1});
+				dp[i][j] = dp[i - 1][j - 1] + 2;
+			dp[i][j] = max({0, dp[i][j], dp[i][j - 1] - 1, dp[i - 1][j] - 1, dp[i - 1][j - 1] - 2});
 			ans = max(ans, dp[i][j]);
 		}
 	}
