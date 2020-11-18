fe2465c8360a1c388df6783759b411df76d63443
Sahil <sahilkhan0307.sk@gmail.com>
2020-11-18 19:04:00 +0530

    Add solution for problem `D - Swaps in Permutation` Link: https://codeforces.com/contest/691/problem/D Tags: dfs and similar, dsu, math, *1700 Ref: https://codeforces.com/contest/691/submission/98815284

---
+++ codeforces/691/D.cpp
@@ -0,0 +1,161 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+	return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const set<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T1, typename T2>
+inline ostream &operator<<(ostream &os, const map<T1, T2> &t) {
+	for (auto i : t) os << i.first << " : " << i.second << endl;
+	return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+	for (T &t : v) is >> t;
+	return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, vector<pair<T1, T2>> &v) {
+	for (pair<T1, T2> &t : v) is >> t.first >> t.second;
+	return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+	bool first;
+	string separator;
+	Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+	template <typename ObjectType>
+	Debugger &operator, (const ObjectType &v) {
+		if (!first) cerr << separator;
+		cerr << v;
+		first = false;
+		return *this;
+	}
+	~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+const ll mod = 1e9 + 7;
+
+
+struct dsu {
+public:
+	dsu() : _n(0) {}
+	dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+	int merge(int a, int b) {
+		assert(0 <= a && a < _n);
+		assert(0 <= b && b < _n);
+		int x = leader(a), y = leader(b);
+		if (x == y) return x;
+		if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
+		parent_or_size[x] += parent_or_size[y];
+		parent_or_size[y] = x;
+		return x;
+	}
+
+	bool same(int a, int b) {
+		assert(0 <= a && a < _n);
+		assert(0 <= b && b < _n);
+		return leader(a) == leader(b);
+	}
+
+	int leader(int a) {
+		assert(0 <= a && a < _n);
+		if (parent_or_size[a] < 0) return a;
+		return parent_or_size[a] = leader(parent_or_size[a]);
+	}
+
+	int size(int a) {
+		assert(0 <= a && a < _n);
+		return -parent_or_size[leader(a)];
+	}
+
+	std::vector<std::vector<int>> groups() {
+		std::vector<int> leader_buf(_n), group_size(_n);
+		for (int i = 0; i < _n; i++) {
+			leader_buf[i] = leader(i);
+			group_size[leader_buf[i]]++;
+		}
+		std::vector<std::vector<int>> result(_n);
+		for (int i = 0; i < _n; i++) {
+			result[i].reserve(group_size[i]);
+		}
+		for (int i = 0; i < _n; i++) {
+			result[leader_buf[i]].push_back(i);
+		}
+		result.erase(
+		    std::remove_if(result.begin(), result.end(),
+		[&](const std::vector<int>& v) { return v.empty(); }),
+		result.end());
+		return result;
+	}
+
+private:
+	int _n;
+	std::vector<int> parent_or_size;
+};
+
+
+
+void solve() {
+	ll n, m;
+	cin >> n >> m;
+	vl v(n); cin >> v;
+	dsu d(n);
+	while (m--) {
+		ll a, b;
+		cin >> a >> b;
+		a--, b--;
+		d.merge(a, b);
+	}
+	auto grp = d.groups();
+	for (auto& g : grp) {
+		vi cur;
+		for (auto x : g) cur.pb(v[x]);
+		sort(all(cur), greater<int>());
+		sort(all(g));
+		for (int i = 0; i < g.size(); i++)
+			v[g[i]] = cur[i];
+	}
+	cout << v << endl;
+}
+
+int main()
+{
+	fast;
+	ll T = 1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+	return 0;
+}
