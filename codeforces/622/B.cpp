f0624d7604775b77871ef4339761a0cd0d59adf1
Sahil <sahilkhan0307.sk@gmail.com>
2020-12-26 18:40:00 +0530

    Add solution for problem `B - The Time` Link: https://codeforces.com/contest/622/problem/B Tags: implementation, *900 Ref: https://codeforces.com/contest/622/submission/102397847

---
+++ codeforces/622/B.cpp
@@ -0,0 +1,83 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+	return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+	for (T &t : v) is >> t;
+	return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, pair<T1, T2> &t) {
+	is >> t.first >> t.second;
+	return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+	bool first;
+	string separator;
+	Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+	template <typename ObjectType>
+	Debugger &operator, (const ObjectType &v) {
+		if (!first) cerr << separator;
+		cerr << v;
+		first = false;
+		return *this;
+	}
+	~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+const ll mod = 1e9 + 7;
+
+void solve() {
+	string s; cin >> s;
+	ll a; cin >> a;
+	ll h = (s[0] - '0') * 10 + s[1] - '0',
+	   m = (s[3] - '0') * 10 + s[4] - '0';
+	h = (h + (m + a) / 60) % 24;
+	m = (m + a) % 60;
+	if (!h) cout << "00";
+	else if (log10(h) < 1) cout << "0" << h;
+	else cout << h;
+	cout << ":";
+	if (!m) cout << "00";
+	else if (log10(m) < 1) cout << "0" << m;
+	else cout << m;
+}
+
+int main()
+{
+	fast;
+	ll T = 1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+	return 0;
+}
