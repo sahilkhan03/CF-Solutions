a6dc626680fddaefcd040da0928d10b9d6a3c1dc
Sahil <sahilkhan0307.sk@gmail.com>
2021-01-09 18:34:00 +0530

    Add solution for problem `C - Pearls in a Row` Link: https://codeforces.com/contest/620/problem/C Tags: greedy, *1500 Ref: https://codeforces.com/contest/620/submission/103880994

---
+++ codeforces/620/C.cpp
@@ -0,0 +1,93 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+	return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+	for (T &t : v) is >> t;
+	return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, pair<T1, T2> &t) {
+	is >> t.first >> t.second;
+	return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+	bool first;
+	string separator;
+	Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+	template <typename ObjectType>
+	Debugger &operator, (const ObjectType &v) {
+		if (!first) cerr << separator;
+		cerr << v;
+		first = false;
+		return *this;
+	}
+	~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+const ll mod = 1e9 + 7;
+
+void solve() {
+	ll n; cin >> n;
+	vl v(n); cin >> v;
+	map<ll, ll> m;
+	vector<pl> ans;
+	ll ls = 1;
+	for (int i = 0; i < n; i++) {
+		if (m.find(v[i]) != m.end()) {
+			ans.pb({ls, i + 1});
+			ls = i + 2;
+			m.clear();
+		}
+		else m[v[i]] = i + 1;
+	}
+	if (ans.empty()) cout << -1 << endl;
+	else  {
+		if (ans.back().S != n) {
+			ls = ans.back().F;
+			ans.pop_back();
+			ans.pb({ls, n});
+		};
+		cout << ans.size() << endl;
+		for (auto x : ans) cout << x << endl;
+	}
+}
+
+int main()
+{
+	fast;
+	ll T = 1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+	return 0;
+}
