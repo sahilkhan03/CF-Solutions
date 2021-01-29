ec093d4ba295cdff5b0af5417e6bcd7ad0fda993
Sahil <sahilkhan0307.sk@gmail.com>
2021-01-29 11:05:00 +0530

    Add solution for problem `D - Nauuo and Circle` Link: https://codeforces.com/contest/1173/problem/D Tags: combinatorics, dfs and similar, dp, geometry, trees, *1900 Ref: https://codeforces.com/contest/1173/submission/105806436

---
+++ codeforces/1173/D.cpp
@@ -0,0 +1,90 @@
+#pragma GCC optimize("Ofast")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+	return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+	for (T &t : v) is >> t;
+	return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, pair<T1, T2> &t) {
+	is >> t.first >> t.second;
+	return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+	bool first;
+	string separator;
+	Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+	template <typename ObjectType>
+	Debugger &operator, (const ObjectType &v) {
+		if (!first) cerr << separator;
+		cerr << v;
+		first = false;
+		return *this;
+	}
+	~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+const ll mod = 998244353;
+
+void solve() {
+	int n; cin >> n;
+	vector<vi> v(n);
+	for (int i = 0; i < n - 1; i++) {
+		int a, b;
+		cin >> a >> b;
+		v[a - 1].pb(b - 1);
+		v[b - 1].pb(a - 1);
+	}
+	vl fact(2e5 + 5);
+	fact[0] = 1;
+	for (int i = 1; i < 2e5 + 5; i++) fact[i] = fact[i - 1] * i % mod;
+	ll ans = n * fact[v[0].size()] % mod;
+	queue<pl> q;
+	for (auto x : v[0]) q.push({x, 0});
+	while (!q.empty()) {
+		auto [i, p] = q.front(); q.pop();
+		(ans *= fact[v[i].size()]) %= mod;
+		for (auto x : v[i]) if (x != p) q.push({x, i});
+	}
+	cout << ans << endl;
+}
+
+int main()
+{
+	fast;
+	int T = 1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+	return 0;
+}
