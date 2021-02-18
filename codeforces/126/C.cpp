34c2480d5b5607b0bdc0916e04e043447773f1e9
sahil <sahilkhan0307.sk@gmail.com>
2021-02-18 10:33:00 +0530

    Add solution for problem `C - E-reader Display` Link: https://codeforces.com/contest/126/problem/C Tags: constructive algorithms, greedy, *2000 Ref: https://codeforces.com/contest/126/submission/107742754

---
+++ codeforces/126/C.cpp
@@ -0,0 +1,95 @@
+#pragma GCC optimize("Ofast")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+	return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+	for (T &t : v) is >> t;
+	return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, pair<T1, T2> &t) {
+	is >> t.first >> t.second;
+	return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+	bool first;
+	string separator;
+	Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+	template <typename ObjectType>
+	Debugger &operator, (const ObjectType &v) {
+		if (!first) cerr << separator;
+		cerr << v;
+		first = false;
+		return *this;
+	}
+	~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+const ll mod = 1e9 + 7;
+
+void solve() {
+	int n; cin >> n;
+	vector<string> s(n);
+	vl col(n), row(n);
+	cin >> s;
+	ll ans = 0;
+	for (int i = 0; i < n; i++) {
+		int j, use = 0;
+		for (j = n - 1; j > i; j--) {
+			int cur = s[i][j] - '0' + use + col[j];
+			if (cur & 1) {
+				use++; ans++;
+				col[j]++;
+			}
+		}
+		int use1 = 0;
+		for (j = n - 1; j > i; j--) {
+			int cur = s[j][i] - '0' + row[j] + use1;
+			if (cur & 1) {
+				use1++; ans++;
+				row[j]++;
+			}
+		}
+		ans += (use + use1 + s[i][i] - '0' + row[i] + col[i]) % 2;
+	}
+	cout << ans << endl;
+}
+
+int main()
+{
+	fast;
+	int T = 1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+	return 0;
+}
