0503af9f62b766b9bb990d9ea8c5aac8e0992673
sahil <sahilkhan0307.sk@gmail.com>
2021-04-03 20:18:00 +0530

    Add solution for problem `B - Flip the Bits` Link: https://codeforces.com/contest/1504/problem/B Tags: constructive algorithms, greedy, implementation, math, *1200 Ref: https://codeforces.com/contest/1504/submission/111878720

---
+++ codeforces/1504/B.cpp
@@ -0,0 +1,88 @@
+#pragma GCC optimize("Ofast")
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+#define pb push_back
+#define pl pair<ll, ll>
+#define vl vector<ll>
+#define vi vector<int>
+#define endl '\n'
+
+template <typename T, typename TT>
+inline ostream &operator<<(ostream &os, const pair<T, TT> &t) {
+	return os << t.first << " " << t.second;
+}
+template <typename T>
+inline ostream &operator<<(ostream &os, const vector<T> &t) {
+	for (auto i : t) os << i << " ";
+	return os;
+}
+template <typename T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+	for (T &t : v) is >> t;
+	return is;
+}
+template <typename T1, typename T2>
+inline istream &operator>>(istream &is, pair<T1, T2> &t) {
+	is >> t.first >> t.second;
+	return is;
+}
+
+#ifdef LOCAL
+#define debug(args...) (Debugger()), args
+class Debugger {
+public:
+	bool first;
+	string separator;
+	Debugger(const string &_separator = ", ") : first(true), separator(_separator) {}
+	template <typename ObjectType>
+	Debugger &operator, (const ObjectType &v) {
+		if (!first) cerr << separator;
+		cerr << v;
+		first = false;
+		return *this;
+	}
+	~Debugger() { cerr << endl; }
+};
+#else
+#define debug(args...)
+#endif
+
+const ll mod = 1e9 + 7;
+
+void solve() {
+	ll n; cin >> n;
+	string a, b;
+	cin >> a >> b;
+	int f = 0, c = accumulate(all(a), 0ll) - ('0') * n;
+	for (int i = n - 1; i >= 0; i--) {
+		int cur = a[i] - '0';
+		cur ^= f;
+		if (cur != b[i] - '0') {
+			if (c != i + 1 - c) {
+				cout << "NO" << endl;
+				return;
+			}
+			f ^= 1;
+			cur ^= 1;
+		}
+		c -= cur;
+	}
+	cout << "YES" << endl;
+}
+
+int main()
+{
+	fast;
+	int T = 1;
+	cin >> T;
+	while (T--) {
+		solve();
+	}
+	return 0;
+}
