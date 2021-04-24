6d179e4f0fef24385706f4629f6d887bd497089b
sahil <sahilkhan0307.sk@gmail.com>
2021-04-24 13:02:00 +0530

    Add solution for problem `D - Cut and Stick` Link: https://codeforces.com/contest/1514/problem/D Tags: binary search, data structures, greedy, implementation, sortings, *2000 Ref: https://codeforces.com/contest/1514/submission/114085418

--- codeforces/1514/D.cpp
+++ codeforces/1514/D.cpp
@@ -55,70 +55,26 @@ public:
 
 const ll mod = 1e9 + 7;
 
-vector<vi> pos(3e5 + 5);
-
-ll freq(int x, int l, int r) {
-	return upper_bound(all(pos[x]), r) - lower_bound(all(pos[x]), l);
-}
-
-struct SegTree
-{
-	int size;
-	vl val;
-	SegTree(int n) {
-		size = 1;
-		while (size < n) size *= 2;
-		val = vl(2 * size, 0);
-	}
-	SegTree(vl &v) {
-		size = 1;
-		while (size < v.size()) size *= 2;
-		val = vl(2 * size, 0);
-		build(v);
-	}
-	void build(vl &v, int x, int lx, int rx) {
-		if (rx - lx == 1) {
-			if (lx < v.size())
-				val[x] = v[lx];
-			return;
-		}
-		int mid = (lx + rx) / 2;
-		build(v, 2 * x + 1, lx, mid);
-		build(v, 2 * x + 2, mid, rx);
-		val[x] = (freq(val[2 * x + 1], lx, rx - 1) > freq(val[2 * x + 2], lx, rx - 1) ? val[2 * x + 1] : val[2 * x + 2]);
-	}
-	void build(vl &v) {
-		build(v, 0, 0, size);
-	}
-	ll query(int l, int r, int x, int lx, int rx) {
-		if (lx >= r or rx <= l) return 0;
-		if (l <= lx and rx <= r) return val[x];
-		int mid = (lx + rx) / 2;
-		int a = query(l, r, 2 * x + 1, lx, mid), b = query(l, r, 2 * x + 2, mid, rx);
-		return (freq(a, l, r - 1) > freq(b, l, r - 1) ? a : b);
-	}
-	ll query(int l, int r) {
-		return query(l, r, 0, 0, size);
-	}
-};
-
 
 void solve() {
 	int n, q;
 	cin >> n >> q;
-	vl arr(n);
+	vector<vi> v(n + 1);
+	vi arr(n);
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
-		pos[arr[i]].pb(i);
+		v[arr[i]].pb(i);
 	}
-	SegTree seg(arr);
+	random_device rd;
 	while (q--) {
 		int l, r, ans = 1;
 		cin >> l >> r;
 		l--, r--;
-		int x = seg.query(l, r + 1);
-		int cnt = freq(x, l, r);
-		if (cnt > (r - l + 1) / 2) ans = 1 + cnt - ((r - l + 1) - cnt + 1);
+		for (int i = 0; i < 40; i++) {
+			int x = arr[uniform_int_distribution<int>(l, r)(rd)];
+			int cnt = upper_bound(all(v[x]), r) - lower_bound(all(v[x]), l);
+			if (cnt > (r - l + 1) / 2) ans = 1 + cnt - ((r - l + 1) - cnt + 1);
+		}
 		cout << ans << endl;
 	}
 }
